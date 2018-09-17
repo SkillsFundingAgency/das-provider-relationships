f9b0ec85284c9b5eb78f4fb99d1cca14675cf155
Elliot Ritchie <elliotritchie@hotmail.com>
2018-09-17 11:49:57 +0100

    Added global error logging

--- src/SFA.DAS.ProviderRelationships.Web/Global.asax.cs
+++ src/SFA.DAS.ProviderRelationships.Web/Global.asax.cs
@@ -1,14 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Microsoft.ApplicationInsights;
+using SFA.DAS.NLog.Logger;
+using SFA.DAS.ProviderRelationships.Web.App_Start;
 
 namespace SFA.DAS.ProviderRelationships.Web
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
@@ -16,6 +18,23 @@ namespace SFA.DAS.ProviderRelationships.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var exception = Server.GetLastError();
+
+            if (exception is HttpException httpException && httpException.GetHttpCode() == (int)HttpStatusCode.NotFound)
+            {
+                return;
+            }
+            
+            var container = StructuremapMvc.StructureMapDependencyScope.Container;
+            var logger = container.GetInstance<ILog>();
+            var telemetryClient = new TelemetryClient();
+
+            logger.Error(exception, "Application error");
+            telemetryClient.TrackException(exception);
         }
     }
 }
