5c89b60e79d44c6a8e9a9d0693811b8a2295d4f4
Phil Davies <github@phildavies.co.uk>
2018-09-18 14:13:33 +0100

    OwinAuthenticationService wip

--- src/SFA.DAS.ProviderRelationships/Authentication/OwinAuthenticationService.cs
+++ src/SFA.DAS.ProviderRelationships/Authentication/OwinAuthenticationService.cs
@@ -3,29 +3,29 @@ using System.Security.Claims;
 using System.Web;
 //using IdentityModel.Client;
 //using SFA.DAS.EmployerUsers.WebClientComponents;
-using SFA.DAS.ProviderRelationships.Configuration;
+//using SFA.DAS.ProviderRelationships.Configuration;
 
 namespace SFA.DAS.ProviderRelationships.Authentication
 {
+    //todo: clean up the commented out code when we know it's not needed
+
     /// <remarks>
     /// in EmployerCommitments this class still has its old name of OwinWrapper
     /// </remarks>
     public class OwinAuthenticationService : IAuthenticationService
     {
         //todo: extract authentication config into own class, and use to compose service config: that way we could put this class in a common package, and use it in EAS, employercommitments and here
-        private readonly ProviderRelationshipsConfiguration _configuration;
+        //private readonly ProviderRelationshipsConfiguration _configuration;
 
-        public OwinAuthenticationService(ProviderRelationshipsConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
+        //public OwinAuthenticationService(ProviderRelationshipsConfiguration configuration)
+        //{
+        //    _configuration = configuration;
+        //}
 
         /// <returns>Claim value, or null if not found (EAS & EC return "" if not found)</returns>
         public string GetCurrentUserClaimValue(string key)
         {
-            //var claimIdentity = ((ClaimsIdentity)HttpContext.Current.User.Identity).Claims.FirstOrDefault(c => c.Type == key);
-            //return claimIdentity == null ? "" : claimIdentity.Value;
-
+            //todo: do we throw if not found and still have a Try.. method? or return null if not found?
             return ((ClaimsIdentity)HttpContext.Current.User.Identity).Claims.FirstOrDefault(c => c.Type == key)?.Value;
         }
 
