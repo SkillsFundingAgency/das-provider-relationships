2fbb6d9cd1733268a87083b9aa7625b2029f5128
Phil Davies <github@phildavies.co.uk>
2018-09-18 14:47:49 +0100

    config changes

--- src/SFA.DAS.ProviderRelationships/Configuration/ClaimIdentifierConfiguration.cs
+++ src/SFA.DAS.ProviderRelationships/Configuration/ClaimIdentifierConfiguration.cs
@@ -4,9 +4,9 @@
     {
         public string ClaimsBaseUrl { get; set; }
         public string Id { get; set; }
-        public string GivenName { get; set; }
-        //todo: fixed spelling, change any copied config settings
-        public string FamilyName { get; set; }
+        //public string GivenName { get; set; }
+        ////todo: fixed spelling, change any copied config settings
+        //public string FamilyName { get; set; }
         public string Email { get; set; }
         public string DisplayName { get; set; }
     }
--- src/SFA.DAS.ProviderRelationships/Configuration/ProviderRelationshipsConfiguration.cs
+++ src/SFA.DAS.ProviderRelationships/Configuration/ProviderRelationshipsConfiguration.cs
@@ -1,10 +1,39 @@
 ﻿namespace SFA.DAS.ProviderRelationships.Configuration
 {
+    /// <example>
+    /// {
+    ///   "Identity": {
+    ///     "ClientId": "",
+    ///     "ClientSecret": "",
+    ///     "BaseAddress": "",
+    ///     "AuthorizeEndPoint": "",
+    ///     "LogoutEndpoint": "",
+    ///     "TokenEndpoint": "",
+    ///     "UserInfoEndpoint": "",
+    ///     "UseCertificate": true,
+    ///     "Scopes": "",
+    ///     "ChangePasswordLink": "",
+    ///     "ChangeEmailLink": "",
+    ///     "RegisterLink": "",
+    ///     "AccountActivationUrl": "",
+    ///     "ClaimIdentifierConfiguration": {
+    ///       "ClaimsBaseUrl": "",
+    ///       "Id": "",
+    ///       "Email": "",
+    ///       "DisplayName": ""
+    ///     }
+    ///   },
+    ///   "AllowedHashstringCharacters": "",
+    ///   "Hashstring" : "",
+    ///   "NServiceBusLicense": "",
+    ///   "ServiceBusConnectionString": ""
+    /// }
+    /// </example>>
     public class ProviderRelationshipsConfiguration
     {
         public string AllowedHashstringCharacters { get; set; }
-        public string DatabaseConnectionString { get; set; }
         public string Hashstring { get; set; }
+        //public string DatabaseConnectionString { get; set; }
         public string NServiceBusLicense { get; set; }
         public string ServiceBusConnectionString { get; set; }
         public IdentityServerConfiguration Identity { get; set; }
