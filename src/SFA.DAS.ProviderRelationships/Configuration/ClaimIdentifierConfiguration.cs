b00939a57ea56eb39b2b45191fd4be3a977b849d
Phil Davies <github@phildavies.co.uk>
2018-09-26 13:05:36 +0100

    remove comments only

--- src/SFA.DAS.ProviderRelationships/Configuration/ClaimIdentifierConfiguration.cs
+++ src/SFA.DAS.ProviderRelationships/Configuration/ClaimIdentifierConfiguration.cs
@@ -6,9 +6,6 @@ namespace SFA.DAS.ProviderRelationships.Configuration
     {
         public string ClaimsBaseUrl { get; set; }
         public string Id { get; set; }
-        //public string GivenName { get; set; }
-        ////todo: fixed spelling, change any copied config settings
-        //public string FamilyName { get; set; }
         public string Email { get; set; }
         public string DisplayName { get; set; }
     }
