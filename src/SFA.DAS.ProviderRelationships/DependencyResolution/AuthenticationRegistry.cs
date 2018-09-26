8d4f0c5e4f465b3d053e304aae5b1e55f42c893f
Phil Davies <github@phildavies.co.uk>
2018-09-26 08:59:13 +0100

    remove commented out code only

--- src/SFA.DAS.ProviderRelationships/DependencyResolution/AuthenticationRegistry.cs
+++ src/SFA.DAS.ProviderRelationships/DependencyResolution/AuthenticationRegistry.cs
@@ -8,12 +8,6 @@ namespace SFA.DAS.ProviderRelationships.DependencyResolution
     {
         public AuthenticationRegistry()
         {
-            //var providerRelationshipsConfig = ConfigurationHelper.GetConfiguration<ProviderRelationshipsConfiguration>("SFA.DAS.ProviderRelationships");
-
-            //For<IIdentityServerConfiguration>().Use(() => providerRelationshipsConfig.Identity).Singleton();
-            ////todo: change consumers of this to accept IIdentityServerConfiguration instead?
-            //For<IClaimIdentifierConfiguration>().Use(() => providerRelationshipsConfig.Identity.ClaimIdentifierConfiguration).Singleton();
-
             For<ConfigurationFactory>().Use<IdentityServerConfigurationFactory>();
         }
     }
