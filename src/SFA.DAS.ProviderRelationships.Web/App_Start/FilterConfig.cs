0cd1d2bf2d4b1c83ed7cc425a75f14a55c6a488b
Phil Davies <github@phildavies.co.uk>
2018-10-01 08:40:20 +0100

    move email and password link generation into filter (resolves services and calculates links each request: will change to only calc once)

--- src/SFA.DAS.ProviderRelationships.Web/App_Start/FilterConfig.cs
+++ src/SFA.DAS.ProviderRelationships.Web/App_Start/FilterConfig.cs
@@ -1,5 +1,7 @@
 ﻿using SFA.DAS.Validation.Mvc;
 using System.Web.Mvc;
+using SFA.DAS.ProviderRelationships.Authentication;
+using SFA.DAS.ProviderRelationships.Configuration;
 
 namespace SFA.DAS.ProviderRelationships.Web
 {
@@ -8,6 +10,8 @@ namespace SFA.DAS.ProviderRelationships.Web
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.AddValidationFilter();
+
+            filters.Add(new AccountLinksFilter(() => (DependencyResolver.Current.GetService<ProviderRelationshipsConfiguration>(), DependencyResolver.Current.GetService<IAuthenticationUrls>()) ));
         }
     }
 }
---
+++ src/SFA.DAS.ProviderRelationships/Authentication/AccountLinksFilter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web.Mvc;
+using SFA.DAS.ProviderRelationships.Configuration;
+
+namespace SFA.DAS.ProviderRelationships.Authentication
+{
+    //attribute as global filter?
+    public class AccountLinksFilter : ActionFilterAttribute
+    {
+        private readonly Func<(ProviderRelationshipsConfiguration, IAuthenticationUrls)> _provideDependencies;
+
+        public AccountLinksFilter(Func<(ProviderRelationshipsConfiguration, IAuthenticationUrls)> provideDependencies)
+        {
+            _provideDependencies = provideDependencies;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var (providerRelationshipsConfig, authenticationUrls) = _provideDependencies();
+
+            var urlHelper = new UrlHelper();
+            // the second interpolated expression is the return url (we send them back to MA)
+            filterContext.Controller.ViewBag.ChangePasswordLink = $"{authenticationUrls.ChangePasswordLink}{urlHelper.Encode(providerRelationshipsConfig.EmployerPortalBaseUrl.TrimEnd('/') + "/service/password/change")}";
+            filterContext.Controller.ViewBag.ChangeEmailLink = $"{authenticationUrls.ChangeEmailLink}{urlHelper.Encode(providerRelationshipsConfig.EmployerPortalBaseUrl.TrimEnd('/') + "/service/email/change")}";
+        }
+    }
+}
