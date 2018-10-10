112f40c697685e4250da8879b3096a39e9bf905f
Phil Davies <github@phildavies.co.uk>
2018-10-10 11:40:51 +0100

    add publish of all current events to test harness

--- src/SFA.DAS.ProviderRelationships.MessageHandlers.TestHarness/Program.cs
+++ src/SFA.DAS.ProviderRelationships.MessageHandlers.TestHarness/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using SFA.DAS.ProviderRelationships.MessageHandlers.TestHarness.DependencyResolution;
 using NServiceBus;
 using SFA.DAS.EmployerAccounts.Messages.Events;
@@ -14,7 +15,36 @@ namespace SFA.DAS.ProviderRelationships.MessageHandlers.TestHarness
                 try
                 {
                     nServiceBusConfig.Start();
-                    nServiceBusConfig.Endpoint.Publish(new CreatedAccountEvent {AccountId = 1, Name = "name"}).GetAwaiter().GetResult();
+
+                    var userRef = Guid.NewGuid();
+                    const string userName = "Bob Loblaw";
+                    const long accountId = 1;
+                    const long accountLegalEntityId = 5;
+                    const string accountPublicHashedId = "ACCPUB";
+                    const string accountLegalEntityPublicHashedId = "ALEPUB";
+                    const string originalAccountName = "Account Name";
+                    const string updatedAccountName = "New Account Name";
+                    const long legalEntityId = 8;
+                    const string originalLegalEntityName = "Legal Entity";
+                    const string updatedLegalEntityName = "New Legal Entity";
+                    
+                    //todo: need mechanism to set permissions. new permission created/updates event. who owns? probably employercommitments somewhere
+                    
+                    nServiceBusConfig.Endpoint.Publish(new CreatedAccountEvent 
+                        {AccountId = accountId, PublicHashedId = accountPublicHashedId, Name = originalAccountName, UserName = userName, UserRef = userRef, Created = DateTime.UtcNow}).GetAwaiter().GetResult();
+                    
+                    nServiceBusConfig.Endpoint.Publish(new ChangedAccountNameEvent 
+                        {AccountId = accountId, PreviousName = originalAccountName, CurrentName = updatedAccountName, UserName = userName, UserRef = userRef, Created = DateTime.UtcNow}).GetAwaiter().GetResult();
+                    
+                    // note: no address in created, but in updated
+                    nServiceBusConfig.Endpoint.Publish(new AddedLegalEntityEvent
+                        {AccountLegalEntityId = accountLegalEntityId, AccountLegalEntityPublicHashedId = accountLegalEntityPublicHashedId, OrganisationName = originalLegalEntityName, AccountId = accountId, LegalEntityId = legalEntityId, AgreementId = 2, UserName = userName, UserRef = userRef, Created = DateTime.UtcNow}).GetAwaiter().GetResult();
+
+                    nServiceBusConfig.Endpoint.Publish(new UpdatedLegalEntityEvent
+                        { AccountLegalEntityId = accountLegalEntityId, Name = updatedLegalEntityName, Address = "New LE Address", UserName = userName, UserRef = userRef, Created = DateTime.UtcNow});
+
+                    nServiceBusConfig.Endpoint.Publish(new RemovedLegalEntityEvent
+                        { AccountLegalEntityId = accountLegalEntityId, OrganisationName = updatedLegalEntityName, AccountId = accountId, LegalEntityId = legalEntityId, AgreementId = 2, AgreementSigned = true, UserName = userName, UserRef = userRef, Created = DateTime.UtcNow});
                 }
                 finally
                 {
