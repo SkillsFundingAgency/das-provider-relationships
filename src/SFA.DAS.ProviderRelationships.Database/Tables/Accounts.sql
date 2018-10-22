594c1294f93a28ddd08749163eb53ab96a4dfab3
Phil Davies <github@phildavies.co.uk>
2018-10-22 14:11:06 +0100

    fix bad alignment in PR (spaces -> tab only)

--- src/SFA.DAS.ProviderRelationships.Database/Tables/Accounts.sql
+++ src/SFA.DAS.ProviderRelationships.Database/Tables/Accounts.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[Accounts]               -- Employer_Account Data Type
 (                                           
-	[Id] BIGINT NOT NULL,                   -- bigint
-	[Name] NVARCHAR(100) NOT NULL,          -- nvarchar(100)
-	[Created] DateTime2 NOT NULL,
-	[Updated] DateTime2 NULL,
+    [Id] BIGINT NOT NULL,                   -- bigint
+    [Name] NVARCHAR(100) NOT NULL,          -- nvarchar(100)
+    [Created] DateTime2 NOT NULL,
+    [Updated] DateTime2 NULL,
     CONSTRAINT [PK_Accounts] PRIMARY KEY CLUSTERED ([Id] ASC),
 )
--- src/SFA.DAS.ProviderRelationships.Database/Tables/Permissions.sql
+++ src/SFA.DAS.ProviderRelationships.Database/Tables/Permissions.sql
@@ -1,12 +1,12 @@
 ﻿CREATE TABLE [dbo].[Permissions]
 (
-	[Id] INT NOT NULL IDENTITY, 
-	[Type] SMALLINT NOT NULL, 
-	[AccountLegalEntityId] BIGINT NOT NULL, 
-	[Ukprn] BIGINT NOT NULL,
-	CONSTRAINT [PK_Permissions] PRIMARY KEY CLUSTERED ([Id] ASC),
-	--todo: need to make sure cascade delete is right way around!
-	CONSTRAINT [FK_Permissions_AccountLegalEntities_AccountLegalEntityId] FOREIGN KEY ([AccountLegalEntityId]) REFERENCES [AccountLegalEntities]([Id]) ON DELETE CASCADE,
+    [Id] INT NOT NULL IDENTITY, 
+    [Type] SMALLINT NOT NULL, 
+    [AccountLegalEntityId] BIGINT NOT NULL, 
+    [Ukprn] BIGINT NOT NULL,
+    CONSTRAINT [PK_Permissions] PRIMARY KEY CLUSTERED ([Id] ASC),
+    --todo: need to make sure cascade delete is right way around!
+    CONSTRAINT [FK_Permissions_AccountLegalEntities_AccountLegalEntityId] FOREIGN KEY ([AccountLegalEntityId]) REFERENCES [AccountLegalEntities]([Id]) ON DELETE CASCADE,
     CONSTRAINT [FK_Permissions_ToProviders] FOREIGN KEY ([Ukprn]) REFERENCES [Providers]([Ukprn]),
     INDEX [IX_Permissions_AccountLegalEntityId] NONCLUSTERED ([AccountLegalEntityId]),
     INDEX [IX_Permissions_Ukprn] NONCLUSTERED ([Ukprn])    
