cce6cb1997d185a30e62a7b46cbd1702a9ab8481
Phil Davies <github@phildavies.co.uk>
2018-10-22 11:12:41 +0100

    introduce clustered identity id for tables with cluster-unfriendly natural primary keys

--- src/SFA.DAS.ProviderRelationships.Database/Tables/Providers.sql
+++ src/SFA.DAS.ProviderRelationships.Database/Tables/Providers.sql
@@ -1,8 +1,10 @@
 ﻿CREATE TABLE [dbo].[Providers]
 (
+	[Id] INT NOT NULL IDENTITY,
 	[Ukprn] BIGINT NOT NULL,
 	[Name] nvarchar(100) NOT NULL,  -- 100, max? what's source?
 	[Created] DateTime2 NOT NULL,
 	[Updated] DateTime2 NULL,
-    CONSTRAINT [PK_Providers] PRIMARY KEY CLUSTERED ([Ukprn] ASC)
+    CONSTRAINT [PK_Providers] PRIMARY KEY NONCLUSTERED ([Ukprn] ASC),
+    CONSTRAINT [AK_Providers_Id] UNIQUE CLUSTERED ([Id] ASC) 
 )
