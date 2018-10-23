b31cf6b74e91b68ffcbaf9c82114b45ab20d03a4
Phil Davies <github@phildavies.co.uk>
2018-10-23 12:57:33 +0100

    consistent casing

--- src/SFA.DAS.ProviderRelationships.Database/Tables/Accounts.sql
+++ src/SFA.DAS.ProviderRelationships.Database/Tables/Accounts.sql
@@ -2,7 +2,7 @@
 (                                           
     [Id] BIGINT NOT NULL,                   -- bigint
     [Name] NVARCHAR(100) NOT NULL,          -- nvarchar(100)
-    [Created] DateTime2 NOT NULL,
-    [Updated] DateTime2 NULL,
+    [Created] DATETIME2 NOT NULL,
+    [Updated] DATETIME2 NULL,
     CONSTRAINT [PK_Accounts] PRIMARY KEY CLUSTERED ([Id] ASC),
 )
--- src/SFA.DAS.ProviderRelationships.Database/Tables/Providers.sql
+++ src/SFA.DAS.ProviderRelationships.Database/Tables/Providers.sql
@@ -2,9 +2,9 @@
 (
     [Id] INT NOT NULL IDENTITY,
     [Ukprn] BIGINT NOT NULL,
-    [Name] nvarchar(100) NOT NULL,  -- 100, max? what's source?
-    [Created] DateTime2 NOT NULL,
-    [Updated] DateTime2 NULL,
+    [Name] NVARCHAR(100) NOT NULL,  -- 100, max? what's source?
+    [Created] DATETIME2 NOT NULL,
+    [Updated] DATETIME2 NULL,
     CONSTRAINT [PK_Providers] PRIMARY KEY NONCLUSTERED ([Ukprn] ASC),
     CONSTRAINT [AK_Providers_Id] UNIQUE CLUSTERED ([Id] ASC) 
 )
