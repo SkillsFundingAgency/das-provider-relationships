db16fcbe979a61904e235be1f54dcb049b33600d
Phil Davies <github@phildavies.co.uk>
2018-10-24 12:47:40 +0100

    remove (clustered identity) id column from Providers and cluster on ukprn instead

--- src/SFA.DAS.ProviderRelationships.Database/Tables/Providers.sql
+++ src/SFA.DAS.ProviderRelationships.Database/Tables/Providers.sql
@@ -1,10 +1,8 @@
 ﻿CREATE TABLE [dbo].[Providers]
 (
-    [Id] INT NOT NULL IDENTITY,
     [Ukprn] BIGINT NOT NULL,
     [Name] NVARCHAR(100) NOT NULL,  -- 100, max? what's source?
     [Created] DATETIME2 NOT NULL,
     [Updated] DATETIME2 NULL,
-    CONSTRAINT [PK_Providers] PRIMARY KEY NONCLUSTERED ([Ukprn] ASC),
-    CONSTRAINT [AK_Providers_Id] UNIQUE CLUSTERED ([Id] ASC) 
+    CONSTRAINT [PK_Providers] PRIMARY KEY CLUSTERED ([Ukprn] ASC)
 )
