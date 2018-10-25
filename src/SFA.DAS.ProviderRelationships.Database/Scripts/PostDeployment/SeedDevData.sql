8291aa96914b4710fc6ec3ad4d43fe33e6831527
Phil Davies <github@phildavies.co.uk>
2018-10-25 14:23:22 +0100

    fix table name

--- src/SFA.DAS.ProviderRelationships.Database/Scripts/PostDeployment/SeedDevData.sql
+++ src/SFA.DAS.ProviderRelationships.Database/Scripts/PostDeployment/SeedDevData.sql
@@ -1,4 +1,4 @@
-﻿IF (NOT EXISTS (SELECT 1 FROM [dbo].[Account] WHERE Id = 1))
+﻿IF (NOT EXISTS (SELECT 1 FROM [dbo].[Accounts] WHERE Id = 1))
 BEGIN
     INSERT INTO [dbo].[Accounts] ([Id], [Name], [Created]) VALUES (1, 'Foobar Ltd', GETUTCDATE())
 END
