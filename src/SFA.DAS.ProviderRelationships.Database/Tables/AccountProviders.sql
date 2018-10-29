cf4667119b9f44b7e7b0cd06be7a1d52ca673a5b
Elliot Ritchie <elliotritchie@hotmail.com>
2018-10-29 16:35:33 +0000

    Added unique constraint to AccountProviders table

--- src/SFA.DAS.ProviderRelationships.Database/Tables/AccountProviders.sql
+++ src/SFA.DAS.ProviderRelationships.Database/Tables/AccountProviders.sql
@@ -7,6 +7,7 @@
     CONSTRAINT [PK_AccountProviders] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_AccountProviders_Accounts_AccountId] FOREIGN KEY ([AccountId]) REFERENCES [Accounts]([Id]) ON DELETE CASCADE,
     CONSTRAINT [FK_AccountProviders_Providers_ProviderUkprn] FOREIGN KEY ([ProviderUkprn]) REFERENCES [Providers]([Ukprn]) ON DELETE CASCADE,
+    CONSTRAINT [UK_AccountProviders_AccountId_ProviderUkprn] UNIQUE ([AccountId] ASC, [ProviderUkprn] ASC), 
     INDEX [IX_AccountProviders_AccountId] NONCLUSTERED ([AccountId]),
     INDEX [IX_AccountProviders_ProviderUkprn] NONCLUSTERED ([ProviderUkprn])
 )
