be5c83484759fc16374d915c88c25625b1207b64
Paul Graham <paul.graham@amido.com>
2018-10-30 08:07:23 +0000

    Added tests for ai.clent

--- src/SFA.DAS.ProviderRelationships.Api.Client.UnitTests/ProviderRelationshipServiceTests.cs
+++ src/SFA.DAS.ProviderRelationships.Api.Client.UnitTests/ProviderRelationshipServiceTests.cs
@@ -1,55 +1,51 @@
-﻿//using System.Collections.Generic;
-//using System.Threading;
-//using System.Threading.Tasks;
-//using FluentAssertions;
-//using NUnit.Framework;
-//using SFA.DAS.ProviderRelationships.Api.Client.Application;
-//using SFA.DAS.ProviderRelationships.Document.Repository;
-//using SFA.DAS.ProviderRelationships.ReadStore.Models;
-//using SFA.DAS.ProviderRelationships.Types;
-//using SFA.DAS.Testing;
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.ProviderRelationships.Api.Client.Application;
+using SFA.DAS.ProviderRelationships.Document.Repository;
+using SFA.DAS.ProviderRelationships.ReadStore.Models;
+using SFA.DAS.ProviderRelationships.Types;
+using SFA.DAS.Testing;
 
-//namespace SFA.DAS.ProviderRelationships.Api.Client.UnitTests
-//{
-//    [TestFixture]
-//    public class ProviderRelationshipServiceTests : FluentTest<ProviderRelationshipServiceTestsFixture>
-//    {
-//        [TestCase(1, true)]
-//        [TestCase(2, true)]
-//        [TestCase(3, false)]
-//        public Task Handle_WhenCheckingForAnyProviderRelationshipsWithPermission_ThenShouldReturnCorrectResult(long ukprn, bool result)
-//        {
-//            return RunAsync(f => f.Service.HasRelationshipWithPermission(ukprn, PermissionEnumDto.CreateCohort, CancellationToken.None)), 
-//            (f, r) => r.Should().Be(result));
-//        }
-//    }
+namespace SFA.DAS.ProviderRelationships.Api.Client.UnitTests
+{
+    [TestFixture]
+    public class ProviderRelationshipServiceTests : FluentTest<ProviderRelationshipServiceTestsFixture>
+    {
+        [TestCase(1, true)]
+        [TestCase(2, true)]
+        [TestCase(3, false)]
+        public Task WhenCheckingForAnyProviderRelationshipsWithPermission_ThenShouldReturnCorrectResult(long ukprn, bool result)
+        {
+            return RunAsync(f => f.Service.HasRelationshipWithPermission(ukprn, PermissionEnumDto.CreateCohort, CancellationToken.None), 
+            (f, r) => r.Should().Be(result));
+        }
+    }
 
-//    public class ProviderRelationshipServiceTestsFixture
-//    {
-//        public ProviderRelationshipService Service;
-//        //public Mock<ICosmosQueryWrapper<ProviderPermissions>> CosmosQueryWrapper;
-//        //public ICosmosQueryWrapper<ProviderPermissions> CosmosQueryWrapper;
-//        public IDocumentReadOnlyRepository<ProviderPermissions> ReadRepository  { get; set; }
-//        public List<ProviderPermissions> ProviderRelationships { get; set; }
+    public class ProviderRelationshipServiceTestsFixture
+    {
+        public ProviderRelationshipService Service;
+        public IDocumentReadOnlyRepository<ProviderPermissions> ReadRepository { get; set; }
+        public List<ProviderPermissions> ProviderRelationships { get; set; }
 
-//        public ProviderRelationshipServiceTestsFixture()
-//        {
+        public ProviderRelationshipServiceTestsFixture()
+        {
 
-//            ProviderRelationships = new List<ProviderPermissions>
-//            {
-//                new ProviderPermissions { Ukprn = 1, GrantPermissions = new List<GrantPermission>( new List<GrantPermission> {new GrantPermission { Permission = PermissionEnumDto.CreateCohort }}) },
-//                new ProviderPermissions { Ukprn = 1, GrantPermissions = new List<GrantPermission>( new List<GrantPermission>()) },
-//                new ProviderPermissions { Ukprn = 2, GrantPermissions = new List<GrantPermission>( new List<GrantPermission> {new GrantPermission { Permission = PermissionEnumDto.CreateCohort }}) },
-//                new ProviderPermissions { Ukprn = 2, GrantPermissions = new List<GrantPermission>( new List<GrantPermission> {new GrantPermission { Permission = PermissionEnumDto.CreateCohort }}) },
-//                new ProviderPermissions { Ukprn = 2, GrantPermissions = new List<GrantPermission>( new List<GrantPermission>()) },
-//                new ProviderPermissions { Ukprn = 3, GrantPermissions = new List<GrantPermission>( new List<GrantPermission>()) }
-//            };
+            ProviderRelationships = new List<ProviderPermissions>
+            {
+                new ProviderPermissions { Ukprn = 1, GrantPermissions = new List<GrantPermission>( new List<GrantPermission> {new GrantPermission { Permission = PermissionEnumDto.CreateCohort }}) },
+                new ProviderPermissions { Ukprn = 1, GrantPermissions = new List<GrantPermission>( new List<GrantPermission>()) },
+                new ProviderPermissions { Ukprn = 2, GrantPermissions = new List<GrantPermission>( new List<GrantPermission> {new GrantPermission { Permission = PermissionEnumDto.CreateCohort }}) },
+                new ProviderPermissions { Ukprn = 2, GrantPermissions = new List<GrantPermission>( new List<GrantPermission> {new GrantPermission { Permission = PermissionEnumDto.CreateCohort }}) },
+                new ProviderPermissions { Ukprn = 2, GrantPermissions = new List<GrantPermission>( new List<GrantPermission>()) },
+                new ProviderPermissions { Ukprn = 3, GrantPermissions = new List<GrantPermission>( new List<GrantPermission>()) }
+            };
 
-//            ReadRepository = new FakeReadOnlyRepository<ProviderPermissions>(ProviderRelationships, null);
-//            //CosmosQueryWrapper = new FakeCosmosQueryWrapper<ProviderPermissions>(ProviderRelationships);
-//            //CosmosQueryWrapper = new Mock<ICosmosQueryWrapper<ProviderPermissions>>();
-//            //CosmosQueryWrapper.Setup()
-//            Handler = new GetHasRelationshipWithPermissionHandler(ReadRepository);
-//        }
-//    }
-//}
+            ReadRepository = new FakeReadOnlyRepository<ProviderPermissions>(ProviderRelationships, null);
+
+            Service = new ProviderRelationshipService(ReadRepository);
+        }
+    }
+}
