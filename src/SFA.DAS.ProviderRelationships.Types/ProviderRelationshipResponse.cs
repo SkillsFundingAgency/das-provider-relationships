d74230c2c6d8c7a3d01a648c7bfbfe031289bad2
Elliot Ritchie <elliotritchie@hotmail.com>
2018-10-30 21:45:22 +0000

    Updated ProviderRelationshipResponse with properties required by PAS

--- src/SFA.DAS.ProviderRelationships.Types/ProviderRelationshipResponse.cs
+++ src/SFA.DAS.ProviderRelationships.Types/ProviderRelationshipResponse.cs
@@ -2,10 +2,12 @@ namespace SFA.DAS.ProviderRelationships.Types
 {
     public class ProviderRelationshipResponse
     {
-        public long Ukprn { get; set; }
         public long EmployerAccountId { get; set; }
-        public string EmployerName { get; set; }
+        public string EmployerAccountPublicHashedId { get; set; }
+        public string EmployerAccountName { get; set; }
+        public long EmployerAccountLegalEntityId { get; set; }
         public string EmployerAccountLegalEntityPublicHashedId { get; set; }
         public string EmployerAccountLegalEntityName { get; set; }
+        public long Ukprn { get; set; }
     }
 }
