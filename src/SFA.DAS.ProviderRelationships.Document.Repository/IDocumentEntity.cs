da397a749cb836e0bab3b74b5bd7eefae0a29ca7
Paul Graham <paul.graham@amido.com>
2018-11-07 15:43:55 +0000

    DocumentEntity add

---
+++ src/SFA.DAS.ProviderRelationships.Document.Repository/IDocumentEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFA.DAS.ProviderRelationships.Document.Repository
+{
+    public interface IDocumentEntity
+    {
+        Guid? Id { get; }
+        string ETag { get; }
+    }
+}
