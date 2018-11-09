dea2cfe498511f29657e5524ba7edd8b015feae7
Phil Davies <github@phildavies.co.uk>
2018-11-09 14:47:15 +0000

    EmployerUrls unit tests

--- src/SFA.DAS.ProviderRelationships.UnitTests/Urls/EmployerUrlsTests.cs
+++ src/SFA.DAS.ProviderRelationships.UnitTests/Urls/EmployerUrlsTests.cs
@@ -10,19 +10,43 @@ namespace SFA.DAS.ProviderRelationships.UnitTests.Urls
     [TestFixture]
     public class EmployerUrlsTests : FluentTest<EmployerUrlsTestsFixture>
     {
-        [TestCaseSource(nameof(NoAccountHashedIdTestCases))]
+        [TestCaseSource(nameof(_noAccountHashedIdTestCases))]
         public void WhenBaseUrlsHaveTrailingSlashAndGettingUrlWithoutAccountHashedId_ThenCorrectUrlShouldBeReturned(Func<EmployerUrls, string> act, string expected)
         {
             Run(f => f.SetBaseUrlsWithTrailingSlash(), f => act(f.EmployerUrls), (f, r) => r.Should().Be($"http://example.com:12345/{expected}"));
         }
 
-        [TestCaseSource(nameof(NoAccountHashedIdTestCases))]
+        [TestCaseSource(nameof(_noAccountHashedIdTestCases))]
         public void WhenBaseUrlsHaveNoTrailingSlashAndGettingUrlWithoutAccountHashedId_ThenCorrectUrlShouldBeReturned(Func<EmployerUrls, string> act, string expected)
         {
             Run(f => f.SetBaseUrlsWithoutTrailingSlash(), f => act(f.EmployerUrls), (f, r) => r.Should().Be($"http://example.com:12345/{expected}"));
         }
 
-        private static object[] NoAccountHashedIdTestCases =
+        [TestCaseSource(nameof(_accountHashedIdTestCases))]
+        public void WhenBaseUrlsHaveTrailingSlashAndGettingUrlWithASuppliedAccountHashedId_ThenCorrectUrlShouldBeReturned(Func<EmployerUrls, string, string> act, string expected)
+        {
+            Run(f => f.SetBaseUrlsWithTrailingSlash(), f => act(f.EmployerUrls, "123456"), (f, r) => r.Should().Be($"http://example.com:12345/accounts/123456/{expected}"));
+        }
+
+        [TestCaseSource(nameof(_accountHashedIdTestCases))]
+        public void WhenBaseUrlsHaveNoTrailingSlashAndGettingUrlWithASuppliedAccountHashedId_ThenCorrectUrlShouldBeReturned(Func<EmployerUrls, string, string> act, string expected)
+        {
+            Run(f => f.SetBaseUrlsWithoutTrailingSlash(), f => act(f.EmployerUrls, "123456"), (f, r) => r.Should().Be($"http://example.com:12345/accounts/123456/{expected}"));
+        }
+
+        [TestCaseSource(nameof(_accountHashedIdTestCases))]
+        public void WhenBaseUrlsHaveTrailingSlashAndGettingUrlWithANullAccountHashedId_ThenCorrectUrlShouldBeReturned(Func<EmployerUrls, string, string> act, string expected)
+        {
+            Run(f => f.SetBaseUrlsWithTrailingSlash(), f => act(f.EmployerUrls, null), (f, r) => r.Should().Be($"http://example.com:12345/accounts/{expected}"));
+        }
+
+        [TestCaseSource(nameof(_accountHashedIdTestCases))]
+        public void WhenBaseUrlsHaveNoTrailingSlashAndGettingUrlWithANullAccountHashedId_ThenCorrectUrlShouldBeReturned(Func<EmployerUrls, string, string> act, string expected)
+        {
+            Run(f => f.SetBaseUrlsWithoutTrailingSlash(), f => act(f.EmployerUrls, null), (f, r) => r.Should().Be($"http://example.com:12345/accounts/{expected}"));
+        }
+        
+        private static object[] _noAccountHashedIdTestCases =
         {
             new object[] { (Func<EmployerUrls, string>)(eu => eu.YourAccounts()), "service/accounts" },
             new object[] { (Func<EmployerUrls, string>)(eu => eu.NotificationSettings()), "settings/notifications" },
@@ -30,6 +54,17 @@ namespace SFA.DAS.ProviderRelationships.UnitTests.Urls
             new object[] { (Func<EmployerUrls, string>)(eu => eu.SignOut()), "service/signout" },
             new object[] { (Func<EmployerUrls, string>)(eu => eu.Help()), "service/help" },
             new object[] { (Func<EmployerUrls, string>)(eu => eu.Privacy()), "service/privacy" }
+        };
+
+        private static object[] _accountHashedIdTestCases =
+        {
+            new object[] { (Func<EmployerUrls, string, string>)((eu, ahid) => eu.RenameAccount(ahid)), "rename" },
+            new object[] { (Func<EmployerUrls, string, string>)((eu, ahid) => eu.YourTeam(ahid)), "teams/view" },
+            new object[] { (Func<EmployerUrls, string, string>)((eu, ahid) => eu.YourOrganisationsAndAgreements(ahid)), "agreements" },
+            new object[] { (Func<EmployerUrls, string, string>)((eu, ahid) => eu.PayeSchemes(ahid)), "schemes" },
+            new object[] { (Func<EmployerUrls, string, string>)((eu, ahid) => eu.Apprentices(ahid)), "apprentices/home" },
+            new object[] { (Func<EmployerUrls, string, string>)((eu, ahid) => eu.PortalHomepage(ahid)), "teams" },
+            new object[] { (Func<EmployerUrls, string, string>)((eu, ahid) => eu.FinanceHomepage(ahid)), "finance" }
         };
     }
     
