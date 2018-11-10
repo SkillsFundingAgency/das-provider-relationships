fa1e2bc3d2b0a39923a07d53f2a3a6da8dc130e9
Paul Graham <paul.graham@amido.com>
2018-11-10 07:18:21 +0000

    added jsonConvertor to enum

--- src/SFA.DAS.ProviderRelationships.Types/Models/Operation.cs
+++ src/SFA.DAS.ProviderRelationships.Types/Models/Operation.cs
@@ -1,5 +1,9 @@
-﻿namespace SFA.DAS.ProviderRelationships.Types.Models
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace SFA.DAS.ProviderRelationships.Types.Models
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum Operation
     {
         CreateCohort = 0
