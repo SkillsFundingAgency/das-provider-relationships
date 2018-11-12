b028d12e7e8c41241d9fb01ba1896b49ac75062f
Elliot Ritchie <elliotritchie@hotmail.com>
2018-11-12 23:39:48 +0000

    Fixed inconsistencies between DB schema and EF config

--- src/SFA.DAS.ProviderRelationships.Database/Tables/Permissions.sql
+++ src/SFA.DAS.ProviderRelationships.Database/Tables/Permissions.sql
@@ -4,7 +4,7 @@
     [AccountProviderId] BIGINT NOT NULL,
     [Operation] SMALLINT NOT NULL,
     CONSTRAINT [PK_Permissions] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_Permissions_AccountProvider] FOREIGN KEY ([AccountProviderId]) REFERENCES [AccountProviders]([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Permissions_AccountProviders_AccountProviderId] FOREIGN KEY ([AccountProviderId]) REFERENCES [AccountProviders]([Id]) ON DELETE CASCADE,
     CONSTRAINT [UK_Permissions_AccountProviderId_Operation] UNIQUE ([AccountProviderId] ASC, [Operation] ASC),
     INDEX [IX_Permissions_AccountProviderId] NONCLUSTERED ([AccountProviderId] ASC)
 )
