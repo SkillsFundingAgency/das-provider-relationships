5b65918997b225d25cfc5f751159183b2f3d7865
Elliot Ritchie <elliotritchie@hotmail.com>
2018-11-14 08:22:37 +0000

    Updated relationships document operations to be stored as numbers rather than strings

--- src/SFA.DAS.ProviderRelationships.Types/Models/Operation.cs
+++ src/SFA.DAS.ProviderRelationships.Types/Models/Operation.cs
@@ -1,9 +1,5 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-
-namespace SFA.DAS.ProviderRelationships.Types.Models
+﻿namespace SFA.DAS.ProviderRelationships.Types.Models
 {
-    [JsonConverter(typeof(StringEnumConverter))]
     public enum Operation
     {
         CreateCohort = 0
