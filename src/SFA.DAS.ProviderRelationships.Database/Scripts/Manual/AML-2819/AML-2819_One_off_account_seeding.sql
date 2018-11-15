904a0b02d98b5b651df635ee910267e6d715eb56
Paul Graham <paul.graham@amido.com>
2018-11-15 14:40:39 +0000

    Updated script to throw error if there are blank public hashed ids and corrected the fields name. Also added a CreatedDate to the script

--- src/SFA.DAS.ProviderRelationships.Database/Scripts/Manual/AML-2819/AML-2819_One_off_account_seeding.sql
+++ src/SFA.DAS.ProviderRelationships.Database/Scripts/Manual/AML-2819/AML-2819_One_off_account_seeding.sql
@@ -14,57 +14,80 @@ Expectations:
 ~27k+ AccountLegalEntities created
 */
 
-:OUT STDOUT 
+--:OUT STDOUT 
 
 SET NOCOUNT ON
 
 declare @MAXINSERT int = 1000 --insert values() batch size (cannot be more than 1000)
 
 --Some table var declarations
+print 'declare @CreateDateTime DATETIME = GETDATE()'
 print 'declare @Accounts table ([AccountId] bigint,[Name] nvarchar(100))'
 print 'declare @AccountLegalEntities table ([AccountLegalEntityId] bigint,[AccountLegalEntityPublicHashedId] nvarchar(6),[AccountId] bigint,[Name] nvarchar(100))'
 
---Accounts
-select
-case (ROW_NUMBER() OVER (ORDER BY a.Id) % @MAXINSERT) when 1 then 'insert into @Accounts ([AccountId],[Name]) values' + char(13) + char(10) else '' end +
-' (' + convert(varchar,[Id]) + ', ' + '''' + replace([Name],'''','''''') + '''' + ')' + 
-case when ((ROW_NUMBER() OVER (ORDER BY a.Id) % @MAXINSERT = 0) OR (ROW_NUMBER() OVER (ORDER BY a.Id) = (select count(1) from [employer_account].[Account]))) then '' else ',' end
-from
-[employer_account].[Account] a
-order by a.Id asc
+BEGIN TRY
+
+	IF(EXISTS(SELECT * FROM [employer_account].[AccountLegalEntity] WHERE PublicHashedId is null))
+	BEGIN
+		THROW 50001, 'Blank Public Hashed Ids', 1
+	END
+
+	--Accounts
+	select
+	case (ROW_NUMBER() OVER (ORDER BY a.Id) % @MAXINSERT) when 1 then 'insert into @Accounts ([AccountId],[Name]) values' + char(13) + char(10) else '' end +
+	' (' + convert(varchar,[Id]) + ', ' + '''' + replace([Name],'''','''''') + '''' + ')' + 
+	case when ((ROW_NUMBER() OVER (ORDER BY a.Id) % @MAXINSERT = 0) OR (ROW_NUMBER() OVER (ORDER BY a.Id) = (select count(1) from [employer_account].[Account]))) then '' else ',' end
+	from
+	[employer_account].[Account] a
+	order by a.Id asc
 	
 
---AccountLegalEntities
-select
-case (ROW_NUMBER() OVER (ORDER BY ale.Id) % @MAXINSERT) when 1 then 'insert into @AccountLegalEntities ([AccountLegalEntityId],[AccountLegalEntityPublicHashedId],[AccountId],[Name]) values' + char(13) + char(10) else '' end +
-' (' + convert(varchar,ale.[Id]) + ', '
-	+ '''' + ale.[PublicHashedId] + '''' + ', '
-	+ convert(varchar,ale.[AccountId]) + ', '
-	+ '''' + replace(ale.[Name],'''','''''') + ''''
-+ ')'  + 
-case when
-	((ROW_NUMBER() OVER (ORDER BY ale.Id) % @MAXINSERT = 0)
-	OR (ROW_NUMBER() OVER (ORDER BY ale.Id) = (select count(1) from [employer_account].[AccountLegalEntity] where PublicHashedId is not null)))
-then '' else ',' end
-from [employer_account].[AccountLegalEntity] ale
-join [employer_account].[LegalEntity] le on le.Id = ale.LegalEntityId
-where ale.PublicHashedId is not null
-order by ale.Id asc
+	--AccountLegalEntities
+	select
+	case (ROW_NUMBER() OVER (ORDER BY ale.Id) % @MAXINSERT) when 1 then 'insert into @AccountLegalEntities ([AccountLegalEntityId],[AccountLegalEntityPublicHashedId],[AccountId],[Name]) values' + char(13) + char(10) else '' end +
+	' (' + convert(varchar,ale.[Id]) + ', '
+		+ '''' + ale.[PublicHashedId] + '''' + ', '
+		+ convert(varchar,ale.[AccountId]) + ', '
+		+ '''' + replace(ale.[Name],'''','''''') + ''''
+	+ ')'  + 
+	case when
+		((ROW_NUMBER() OVER (ORDER BY ale.Id) % @MAXINSERT = 0)
+		OR (ROW_NUMBER() OVER (ORDER BY ale.Id) = (select count(1) from [employer_account].[AccountLegalEntity] where PublicHashedId is not null)))
+	then '' else ',' end
+	from [employer_account].[AccountLegalEntity] ale
+	join [employer_account].[LegalEntity] le on le.Id = ale.LegalEntityId
+	where ale.PublicHashedId is not null
+	order by ale.Id asc
 
---Final inserts
-print '
-insert into Accounts ([AccountId], [Name])
-select a.[AccountId], a.[Name] from @Accounts a
-left join Accounts e on e.[AccountId] = a.[AccountId]
-where e.[AccountId] is null --skip existing
-print ''Inserted '' + convert(varchar,@@ROWCOUNT) + '' Accounts''
-'
+	--Final inserts
+	print '
+	BEGIN TRANSACTION
 
-print '
-insert into AccountLegalEntities([AccountLegalEntityId],[AccountLegalEntityPublicHashedId],[AccountId],[Name])
-select ale.[AccountLegalEntityId], ale.[AccountLegalEntityPublicHashedId], ale.[AccountId], ale.[Name] from @AccountLegalEntities ale
-left join AccountLegalEntities e on e.[AccountLegalEntityId] = ale.[AccountLegalEntityId]
-where e.[AccountLegalEntityId] is null --skip existing
-print ''Inserted '' + convert(varchar,@@ROWCOUNT) + '' AccountLegalEntities''
-print ''Completed''
-'
+	insert into Accounts ([Id], [Name], [Created])
+	select a.[AccountId], a.[Name], @CreateDateTime from @Accounts a
+	left join Accounts e on e.[Id] = a.[AccountId]
+	where e.[Id] is null --skip existing
+	print ''Inserted '' + convert(varchar,@@ROWCOUNT) + '' Accounts''
+	'
+
+	print '
+	insert into AccountLegalEntities([Id],[PublicHashedId],[AccountId],[Name], [Created])
+	select ale.[AccountLegalEntityId], ale.[AccountLegalEntityPublicHashedId], ale.[AccountId], ale.[Name], @CreateDateTime 
+	from @AccountLegalEntities ale
+	left join AccountLegalEntities e on e.[Id] = ale.[AccountLegalEntityId]
+	where e.[Id] is null --skip existing
+	print ''Inserted '' + convert(varchar,@@ROWCOUNT) + '' AccountLegalEntities''
+	print ''Completed''
+
+
+	ROLLBACK TRANSACTION
+	'
+
+END TRY
+BEGIN CATCH
+	PRINT 'Problem, there are blank public hashed Id'
+END CATCH
+
+
+
+
