d4b75c7dd4e11cbfab574b8831b3f5d7e0341004
Elliot Ritchie <elliotritchie@hotmail.com>
2018-11-20 15:52:22 +0000

    Updated data seed script to include Account.PublicHashedId, Account.Created and AccountLegalEntity.Created data and to also ignore AccountLegalEntity records where Deleted is not null

--- src/SFA.DAS.ProviderRelationships.Database/Scripts/Manual/AML-2819/AML-2819_One_off_account_seeding.sql
+++ src/SFA.DAS.ProviderRelationships.Database/Scripts/Manual/AML-2819/AML-2819_One_off_account_seeding.sql
@@ -21,9 +21,8 @@ SET NOCOUNT ON
 declare @MAXINSERT int = 1000 --insert values() batch size (cannot be more than 1000)
 
 --Some table var declarations
-print 'declare @CreateDateTime DATETIME = GETDATE()'
-print 'declare @Accounts table ([AccountId] bigint,[Name] nvarchar(100))'
-print 'declare @AccountLegalEntities table ([AccountLegalEntityId] bigint,[AccountLegalEntityPublicHashedId] nvarchar(6),[AccountId] bigint,[Name] nvarchar(100))'
+print 'declare @Accounts table ([AccountId] bigint,[PublicHashedId] nvarchar(100),[Name] nvarchar(100),[CreatedDate] datetime)'
+print 'declare @AccountLegalEntities table ([AccountLegalEntityId] bigint,[AccountLegalEntityPublicHashedId] nvarchar(6),[AccountId] bigint,[Name] nvarchar(100),[Created] datetime)'
 
 BEGIN TRY
 
@@ -34,8 +33,8 @@ BEGIN TRY
 
 	--Accounts
 	select
-	case (ROW_NUMBER() OVER (ORDER BY a.Id) % @MAXINSERT) when 1 then 'insert into @Accounts ([AccountId],[Name]) values' + char(13) + char(10) else '' end +
-	' (' + convert(varchar,[Id]) + ', ' + '''' + replace([Name],'''','''''') + '''' + ')' + 
+	case (ROW_NUMBER() OVER (ORDER BY a.Id) % @MAXINSERT) when 1 then 'insert into @Accounts ([AccountId],[PublicHashedId],[Name],[CreatedDate]) values' + char(13) + char(10) else '' end +
+	' (' + convert(varchar,[Id]) + ', ' + '''' + convert(varchar,[PublicHashedId]) + '''' + ', ' + '''' + replace([Name],'''','''''') + '''' + ', ' + '''' + convert(varchar,[CreatedDate],121) + '''' + ')' + 
 	case when ((ROW_NUMBER() OVER (ORDER BY a.Id) % @MAXINSERT = 0) OR (ROW_NUMBER() OVER (ORDER BY a.Id) = (select count(1) from [employer_account].[Account]))) then '' else ',' end
 	from
 	[employer_account].[Account] a
@@ -44,11 +43,12 @@ BEGIN TRY
 
 	--AccountLegalEntities
 	select
-	case (ROW_NUMBER() OVER (ORDER BY ale.Id) % @MAXINSERT) when 1 then 'insert into @AccountLegalEntities ([AccountLegalEntityId],[AccountLegalEntityPublicHashedId],[AccountId],[Name]) values' + char(13) + char(10) else '' end +
+	case (ROW_NUMBER() OVER (ORDER BY ale.Id) % @MAXINSERT) when 1 then 'insert into @AccountLegalEntities ([AccountLegalEntityId],[AccountLegalEntityPublicHashedId],[AccountId],[Name],[Created]) values' + char(13) + char(10) else '' end +
 	' (' + convert(varchar,ale.[Id]) + ', '
 		+ '''' + ale.[PublicHashedId] + '''' + ', '
 		+ convert(varchar,ale.[AccountId]) + ', '
-		+ '''' + replace(ale.[Name],'''','''''') + ''''
+		+ '''' + replace(ale.[Name],'''','''''') + '''' + ','
+		+ '''' + convert(varchar,[Created],121) + ''''
 	+ ')'  + 
 	case when
 		((ROW_NUMBER() OVER (ORDER BY ale.Id) % @MAXINSERT = 0)
@@ -57,14 +57,15 @@ BEGIN TRY
 	from [employer_account].[AccountLegalEntity] ale
 	join [employer_account].[LegalEntity] le on le.Id = ale.LegalEntityId
 	where ale.PublicHashedId is not null
+	and ale.Deleted is null
 	order by ale.Id asc
 
 	--Final inserts
 	print '
 	BEGIN TRANSACTION
 
-	insert into Accounts ([Id], [Name], [Created])
-	select a.[AccountId], a.[Name], @CreateDateTime from @Accounts a
+	insert into Accounts ([Id], [PublicHashedId], [Name], [Created])
+	select a.[AccountId], a.[PublicHashedId], a.[Name], a.[CreatedDate] from @Accounts a
 	left join Accounts e on e.[Id] = a.[AccountId]
 	where e.[Id] is null --skip existing
 	print ''Inserted '' + convert(varchar,@@ROWCOUNT) + '' Accounts''
@@ -72,7 +73,7 @@ BEGIN TRY
 
 	print '
 	insert into AccountLegalEntities([Id],[PublicHashedId],[AccountId],[Name], [Created])
-	select ale.[AccountLegalEntityId], ale.[AccountLegalEntityPublicHashedId], ale.[AccountId], ale.[Name], @CreateDateTime 
+	select ale.[AccountLegalEntityId], ale.[AccountLegalEntityPublicHashedId], ale.[AccountId], ale.[Name], ale.[Created] 
 	from @AccountLegalEntities ale
 	left join AccountLegalEntities e on e.[Id] = ale.[AccountLegalEntityId]
 	where e.[Id] is null --skip existing
