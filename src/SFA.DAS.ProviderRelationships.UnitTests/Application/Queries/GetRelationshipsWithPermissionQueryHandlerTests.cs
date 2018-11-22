248003d61e90541e5d6f673a20596cc0958c76d8
Phil Davies <github@phildavies.co.uk>
2018-11-22 15:27:31 +0000

    GetRelationshipsWithPermissionQueryHandlerTests wip

--- src/SFA.DAS.ProviderRelationships.UnitTests/Application/Queries/GetRelationshipsWithPermissionQueryHandlerTests.cs
+++ src/SFA.DAS.ProviderRelationships.UnitTests/Application/Queries/GetRelationshipsWithPermissionQueryHandlerTests.cs
@@ -10,6 +10,7 @@ using SFA.DAS.ProviderRelationships.Application.Queries;
 using SFA.DAS.ProviderRelationships.Data;
 using SFA.DAS.ProviderRelationships.Mappings;
 using SFA.DAS.ProviderRelationships.Models;
+using SFA.DAS.ProviderRelationships.Types.Dtos;
 using SFA.DAS.ProviderRelationships.Types.Models;
 using SFA.DAS.ProviderRelationships.UnitTests.Builders;
 using SFA.DAS.Testing;
@@ -20,14 +21,27 @@ namespace SFA.DAS.ProviderRelationships.UnitTests.Application.Queries
     [Parallelizable]
     public class GetRelationshipsWithPermissionQueryHandlerTests : FluentTest<GetRelationshipsWithPermissionQueryHandlerTestsFixture>
     {
-        [Test, Ignore("not written yet, just want to check in progress")]
+        [Test]
         public Task Handle_WhenHandlingRelationshipsWithPermissionQuery_ThenShouldReturnCorrectGetRelationshipsWithPermissionQueryResult()
         {
-            return RunAsync(f => f.SetAccountProviderLegalEntities(), f => f.Handle(), (f, r) => r.Should().NotBeNull());
-            //todo:
-//                .And.Match<GetRelationshipsWithPermissionQueryResult>(qr =>
-//                    qr.Relationships.Provider.Ukprn == f.Provider.Ukprn &&
-//                    r2.Provider.Name == f.Provider.Name));
+            return RunAsync(f => f.SetAccountProviderLegalEntities(), f => f.Handle(), (f, r) =>
+            {
+                r.Should().NotBeNull();
+                r.Should().BeEquivalentTo(new GetRelationshipsWithPermissionQueryResult(new[] {
+                    new RelationshipDto
+                    {
+                        //todo: why are we returning what was passed in to us?
+                        Ukprn = f.Query.Ukprn,
+                        EmployerAccountId = f.Account.Id,
+                        EmployerAccountPublicHashedId = f.Account.PublicHashedId,
+                        EmployerAccountName = f.Account.Name,
+                        EmployerAccountProviderId = f.AccountProvider.Id,
+                        EmployerAccountLegalEntityId = f.AccountLegalEntity.Id,
+                        EmployerAccountLegalEntityPublicHashedId = f.AccountLegalEntity.PublicHashedId,
+                        EmployerAccountLegalEntityName = f.AccountLegalEntity.Name
+                    }
+                }));
+            });
         }
     }
 
@@ -38,7 +52,6 @@ namespace SFA.DAS.ProviderRelationships.UnitTests.Application.Queries
         public ProviderRelationshipsDbContext Db { get; set; }
         public IConfigurationProvider ConfigurationProvider { get; set; }
         public Account Account { get; set; }
-        public Provider Provider { get; set; }
         public AccountProvider AccountProvider { get; set; }
         public AccountLegalEntity AccountLegalEntity { get; set; }
         public AccountProviderLegalEntity AccountProviderLegalEntity { get; set; }
@@ -58,32 +71,21 @@ namespace SFA.DAS.ProviderRelationships.UnitTests.Application.Queries
             return Handler.Handle(Query, CancellationToken.None);
         }
         
-//        var relationships = await _db.Value.AccountProviderLegalEntities
-//            .Where(aple => aple.AccountProvider.ProviderUkprn == request.Ukprn
-//                           && aple.Permissions.Any(p => p.Operation == request.Operation))
-//            //.OrderBy(ap => ap.Provider.Name)
-//            .Include(aple => aple.AccountProvider)
-//            .ThenInclude(ap => ap.Account)
-//            .Include(aple => aple.AccountLegalEntity)
-//            .ProjectTo<RelationshipDto>(_configurationProvider)
-//            .ToListAsync(cancellationToken);
-        
         public GetRelationshipsWithPermissionQueryHandlerTestsFixture SetAccountProviderLegalEntities()
         {
             Account = new AccountBuilder().WithId(4660117L);
-            //Provider = new ProviderBuilder().WithUkprn(Query.Ukprn).WithName("Provider Name");
             
             AccountProvider = new AccountProviderBuilder()
                 .WithId(49L)
                 .WithAccountId(Account.Id)
-                .WithProviderUkprn(Provider.Ukprn);
+                .WithProviderUkprn(Query.Ukprn);
 
             AccountLegalEntity = new AccountLegalEntityBuilder().WithId(413L).WithName("Legal Entity Name").WithAccountId(Account.Id);
             AccountProviderLegalEntity = new AccountProviderLegalEntityBuilder().WithId(5).WithAccountProviderId(AccountProvider.Id).WithAccountLegalEntityId(AccountLegalEntity.Id);
             Permission = new PermissionBuilder().WithId(3).WithAccountProviderLegalEntityId(AccountProviderLegalEntity.Id).WithOperation(Query.Operation);
             
+            //todo: if these don't clone, we should
             Db.Accounts.Add(Account);
-            //Db.Providers.Add(Provider);
             Db.AccountProviders.Add(AccountProvider);
             Db.AccountLegalEntities.Add(AccountLegalEntity);
             Db.AccountProviderLegalEntities.Add(AccountProviderLegalEntity);
