c153cf2e36026de3ffbc71a42f85eb64a7885c9c
tom-gough <tom@tgsd.co.uk>
2018-11-26 10:37:12 +0000

    Added registry in to fix http client initialisation

--- src/SFA.DAS.ProviderRelationships.Api.Client.TestHarness/DependencyResolution/DefaultRegistry.cs
+++ src/SFA.DAS.ProviderRelationships.Api.Client.TestHarness/DependencyResolution/DefaultRegistry.cs
@@ -1,5 +1,4 @@
 ﻿using SFA.DAS.ProviderRelationships.Api.Client.Configuration;
-using SFA.DAS.ProviderRelationships.ReadStore.Configuration;
 using StructureMap;
 
 namespace SFA.DAS.ProviderRelationships.Api.Client.TestHarness.DependencyResolution
@@ -8,11 +7,6 @@ namespace SFA.DAS.ProviderRelationships.Api.Client.TestHarness.DependencyResolut
     {
         public DefaultRegistry()
         {
-            For<ProviderRelationshipsReadStoreConfiguration>().Use(() => new ProviderRelationshipsReadStoreConfiguration
-            {
-                Uri = "https://localhost:8081",
-                AuthKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
-            });
 
             For<ProviderRelationshipsApiClientConfiguration>().Use(c => new ProviderRelationshipsApiClientConfiguration {
                 ApiBaseUrl = "https://localhost:44308/",
