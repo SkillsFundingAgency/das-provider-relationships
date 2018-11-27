88d8cab66237557141dfc80baa15a3eeb27effbc
Phil Davies <github@phildavies.co.uk>
2018-11-27 14:02:51 +0000

    RelationshipsController unit tests wip

--- src/SFA.DAS.ProviderRelationships.Api.UnitTests/Controllers/Relationships/GetTests.cs
+++ src/SFA.DAS.ProviderRelationships.Api.UnitTests/Controllers/Relationships/GetTests.cs
@@ -1,7 +1,14 @@
+using System.Threading.Tasks;
+using System.Web.Http;
+using MediatR;
+using Moq;
 using NUnit.Framework;
+using SFA.DAS.ProviderRelationships.Api.ActionParameters.Relationships;
+using SFA.DAS.ProviderRelationships.Api.Controllers;
+using SFA.DAS.ProviderRelationships.Types.Models;
 using SFA.DAS.Testing;
 
-namespace SFA.DAS.ProviderRelationships.Api.UnitTests.RelationshipsController
+namespace SFA.DAS.ProviderRelationships.Api.UnitTests.Controllers.Relationships
 {
     [TestFixture]
     [Parallelizable]
@@ -47,5 +54,26 @@ namespace SFA.DAS.ProviderRelationships.Api.UnitTests.RelationshipsController
 
     public class GetTestsFixture
     {
+        public GetRelationshipsParameters GetRelationshipsParameters { get; set; }
+        public Mock<IMediator> Mediator { get; set; }
+        public RelationshipsController RelationshipsController { get; set; }
+
+        public GetTestsFixture()
+        {
+            GetRelationshipsParameters = new GetRelationshipsParameters
+            {
+                Ukprn = 12345678L,
+                Operation = "CreateCohort"
+            };
+
+            Mediator = new Mock<IMediator>();
+            
+            RelationshipsController = new RelationshipsController(Mediator.Object);
+        }
+
+        public async Task<IHttpActionResult> CallGet()
+        {
+            return await RelationshipsController.Get(GetRelationshipsParameters);
+        }
     }
 }
