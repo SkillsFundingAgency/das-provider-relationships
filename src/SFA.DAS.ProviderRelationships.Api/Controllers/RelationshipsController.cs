bbcef9fb42f2a2a8039a49683da3b7b3e340f1d0
Phil Davies <github@phildavies.co.uk>
2018-11-28 09:00:20 +0000

    action error response wip

--- src/SFA.DAS.ProviderRelationships.Api/Controllers/RelationshipsController.cs
+++ src/SFA.DAS.ProviderRelationships.Api/Controllers/RelationshipsController.cs
@@ -1,5 +1,6 @@
-using System;
 using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -8,10 +9,43 @@ using SFA.DAS.ProviderRelationships.Api.ActionParameters.Relationships;
 using SFA.DAS.ProviderRelationships.Api.Authentication;
 using SFA.DAS.ProviderRelationships.Application.Queries;
 using SFA.DAS.ProviderRelationships.Types.Dtos;
-using SFA.DAS.ProviderRelationships.Types.Models;
 
 namespace SFA.DAS.ProviderRelationships.Api.Controllers
 {
+    public class Error
+    {
+        public string ErrorCode { get; }
+        public string Message { get; }
+        
+        public Error(string errorCode = null, string message = null)
+        {
+            ErrorCode = errorCode;
+            Message = message;
+        }
+    }
+    
+    public class ErrorResult : IHttpActionResult
+    {
+        private readonly HttpStatusCode _httpStatusCode;
+        private readonly Error _error;
+        private readonly HttpRequestMessage _request;
+
+        public ErrorResult(HttpStatusCode httpStatusCode, Error error, HttpRequestMessage request)
+        {
+            _httpStatusCode = httpStatusCode;
+            _error = error;
+            _request = request;
+        }
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(new HttpResponseMessage(_httpStatusCode)
+            {
+                Content = new ObjectContent<Error>(_error, new JsonMediaTypeFormatter()),
+                RequestMessage = _request
+            });
+        }
+    }
+    
     [RoutePrefix("relationships")]
     [AuthorizeRemoteOnly(Roles = "Read")]
     public class RelationshipsController : ApiController
@@ -44,7 +78,13 @@ namespace SFA.DAS.ProviderRelationships.Api.Controllers
             if (parameters.Ukprn == null)
             {
                 // logically this would return all relationships (filtered by operation if supplied)
-                throw new HttpResponseException(HttpStatusCode.NotImplemented);
+
+//todo:extension?
+//todo: centralise all error codes, so client's have access
+//many ways to handle errors, so we may want to change this, but for now
+                const string missingUkprnFilterErrorCode = "F99D3D9C-ACEC-4B1A-A722-45223ADE35A0";
+
+                return new ErrorResult(HttpStatusCode.NotImplemented, new Error(missingUkprnFilterErrorCode, "Currently an Ukprn filter needs to be supplied"), Request);
             }
 
             //operation not supplied or value didn't match enum value
