f3fffc514bc0a0bbc6e75f176cda9d9df999d802
Phil Davies <github@phildavies.co.uk>
2018-11-28 13:24:27 +0000

    tidy comments only

--- src/SFA.DAS.ProviderRelationships.Api.Client/Http/RestClientException.cs
+++ src/SFA.DAS.ProviderRelationships.Api.Client/Http/RestClientException.cs
@@ -11,16 +11,8 @@ namespace SFA.DAS.ProviderRelationships.Api.Client.Http
         public string ReasonPhrase { get; }
         public Uri RequestUri { get; }
         public string ErrorResponse { get; }
-        
-//        public RestClientException(HttpStatusCode statusCode, string reasonPhrase, Uri requestUri, string errorResponse)
-//        {
-//            StatusCode = statusCode;
-//            ReasonPhrase = reasonPhrase;
-//            RequestUri = requestUri;
-//            ErrorResponse = errorResponse;
-//        }
 
-        //todo: add proper serialization support?
+        // note, no proper serialization support yet
 
         public RestClientException(HttpResponseMessage httpResponseMessage, string errorResponse)
             : base(GenerateMessage(httpResponseMessage, errorResponse))
