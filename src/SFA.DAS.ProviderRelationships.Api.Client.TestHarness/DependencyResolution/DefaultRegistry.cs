25e90232274b464d52c4b4a243482feb2e3a0714
Phil Davies <github@phildavies.co.uk>
2018-11-28 12:09:54 +0000

    pick up config from storage account for api client test harness

--- src/SFA.DAS.ProviderRelationships.Api.Client.TestHarness/DependencyResolution/DefaultRegistry.cs
+++ src/SFA.DAS.ProviderRelationships.Api.Client.TestHarness/DependencyResolution/DefaultRegistry.cs
@@ -1,4 +1,7 @@
-﻿using SFA.DAS.ProviderRelationships.Api.Client.Configuration;
+﻿using System.Collections.Specialized;
+using SFA.DAS.ProviderRelationships.Api.Client.Configuration;
+using SFA.DAS.ProviderRelationships.Configuration;
+using SFA.DAS.ProviderRelationships.Environment;
 using SFA.DAS.ProviderRelationships.ReadStore.Configuration;
 using StructureMap;
 
@@ -8,29 +11,21 @@ namespace SFA.DAS.ProviderRelationships.Api.Client.TestHarness.DependencyResolut
     {
         public DefaultRegistry()
         {
-            For<ProviderRelationshipsApiClientConfiguration>().Use(() => new ProviderRelationshipsApiClientConfiguration
-            {
-                ApiBaseUrl = "https://localhost:44308",
-                ClientId = "xxx",
-                ClientSecret = "xxx",
-                IdentifierUri = "https://citizenazuresfabisgov.onmicrosoft.com/xxx",
-                Tenant = "citizenazuresfabisgov.onmicrosoft.com"
-            });
-            
             For<ProviderRelationshipsReadStoreConfiguration>().Use(() => new ProviderRelationshipsReadStoreConfiguration
             {
                 Uri = "https://localhost:8081",
                 AuthKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
             });
 
-            For<ProviderRelationshipsApiClientConfiguration>().Use(() => new ProviderRelationshipsApiClientConfiguration 
+            var appSettings = new NameValueCollection
             {
-                ApiBaseUrl = "https://localhost:44308/",
-                ClientId = "xxx",
-                ClientSecret = "xxx",
-                IdentifierUri = "https://citizenazuresfabisgov.onmicrosoft.com/xxx",
-                Tenant = "citizenazuresfabisgov.onmicrosoft.com"
-            });
+                {"ConfigurationStorageConnectionString", "UseDevelopmentStorage=true"},
+                {"EnvironmentName", "LOCAL"}
+            };
+            
+            For<IEnvironment>().Use<Environment.Environment>().Ctor<NameValueCollection>().Is(appSettings).Singleton();
+            For<IConfiguration>().Use<ProviderRelationships.Configuration.Configuration>().Ctor<NameValueCollection>().Is(appSettings);
+            For<ProviderRelationshipsApiClientConfiguration>().Use(c => c.GetInstance<IConfiguration>().Get<ProviderRelationshipsApiClientConfiguration>("SFA.DAS.ProviderRelationships.Api.Client")).Singleton();
         }
     }
 }
