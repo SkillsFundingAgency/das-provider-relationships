614463b04eb9ba547283eb16223d706482cd5b92
tom-gough <tom@tgsd.co.uk>
2018-12-07 14:15:18 +0000

    Removed duplicates from CreatedAccountEventAuditConfiguration

--- src/SFA.DAS.ProviderRelationships/Data/CreatedAccountEventAuditConfiguration.cs
+++ src/SFA.DAS.ProviderRelationships/Data/CreatedAccountEventAuditConfiguration.cs
@@ -17,32 +17,4 @@ namespace SFA.DAS.ProviderRelationships.Data
             builder.Property(e => e.UserRef).IsRequired();
         }
     }
-
-    public class UpdatedPermissionsEventAuditConfiguration : IEntityTypeConfiguration<UpdatedPermissionsEventAudit>
-    {
-        public void Configure(EntityTypeBuilder<UpdatedPermissionsEventAudit> builder)
-        {
-            builder.HasKey(e => e.Id);
-            builder.Property(e => e.TimeLogged).IsRequired().HasColumnType("datetime");
-            builder.Property(e => e.AccountId).IsRequired().HasColumnType("bigint");
-            builder.Property(e => e.UserRef).IsRequired();
-            builder.Property(e => e.Updated).IsRequired().HasColumnType("datetime");
-            builder.Property(e => e.AccountLegalEntityId).IsRequired().HasColumnType("bigint");
-            builder.Property(e => e.AccountProviderId).IsRequired().HasColumnType("bigint");
-            builder.Property(e => e.AccountProviderLegalEntityId).IsRequired().HasColumnType("bigint");
-            builder.Property(e => e.Ukprn).IsRequired().HasColumnType("bigint");
-            builder.Property(e => e.GrantedOperations).HasColumnType("nvarchar(255)");
-        }
-    }
-
-    public class DeletedPermissionsEventAuditConfiguration : IEntityTypeConfiguration<DeletedPermissionsEventAudit>
-    {
-        public void Configure(EntityTypeBuilder<DeletedPermissionsEventAudit> builder)
-        {
-            builder.HasKey(e => e.Id);
-            builder.Property(e => e.Ukprn).IsRequired().HasColumnType("bigint");
-            builder.Property(e => e.Deleted).IsRequired().HasColumnType("datetime");
-            builder.Property(e => e.TimeLogged).IsRequired().HasColumnType("datetime");
-        }
-    }
 }
