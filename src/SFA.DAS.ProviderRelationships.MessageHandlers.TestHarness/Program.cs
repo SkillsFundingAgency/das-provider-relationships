d5b3c213a58e305c20e70667875089c48c1f175e
tom-gough <tom@tgsd.co.uk>
2018-12-13 15:13:48 +0000

    Removed superflous using statement in test harness

--- src/SFA.DAS.ProviderRelationships.MessageHandlers.TestHarness/Program.cs
+++ src/SFA.DAS.ProviderRelationships.MessageHandlers.TestHarness/Program.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Threading.Tasks;
 using SFA.DAS.ProviderRelationships.MessageHandlers.TestHarness.DependencyResolution;
 using SFA.DAS.ProviderRelationships.MessageHandlers.TestHarness.Scenarios;
