46a8e0406db20f9d5cb29878ca434b973f3a8f06
Phil Davies <github@phildavies.co.uk>
2018-12-14 09:30:00 +0000

    tidy up

--- src/SFA.DAS.ProviderRelationships.Api.Client/DependencyResolution/ProviderRelationshipsApiClientRegistry.cs
+++ src/SFA.DAS.ProviderRelationships.Api.Client/DependencyResolution/ProviderRelationshipsApiClientRegistry.cs
@@ -9,9 +9,6 @@ using SFA.DAS.ProviderRelationships.Api.Client.ReadStore.Application.Queries.Has
 using SFA.DAS.ProviderRelationships.Api.Client.ReadStore.Data;
 using SFA.DAS.ProviderRelationships.Api.Client.ReadStore.Mediator;
 using StructureMap;
-using DocumentClientFactory = SFA.DAS.ProviderRelationships.Api.Client.ReadStore.Data.DocumentClientFactory;
-using IDocumentClientFactory = SFA.DAS.ProviderRelationships.Api.Client.ReadStore.Data.IDocumentClientFactory;
-using ProviderRelationshipsReadStoreConfiguration = SFA.DAS.ProviderRelationships.Api.Client.Configuration.ProviderRelationshipsReadStoreConfiguration;
 
 namespace SFA.DAS.ProviderRelationships.Api.Client.DependencyResolution
 {
@@ -19,38 +16,27 @@ namespace SFA.DAS.ProviderRelationships.Api.Client.DependencyResolution
     {
         public ProviderRelationshipsApiClientRegistry()
         {
-            //IncludeRegistry<AutoConfigurationRegistry>();
-            
-            
-//            IncludeRegistry<ReadStoreConfigurationRegistry>();
             IncludeRegistry<AutoConfigurationRegistry>();
-            //todo: main config will contain one of these
-            For<ProviderRelationshipsReadStoreConfiguration>().Use(c => c.GetInstance<ITableStorageConfigurationService>().Get<ProviderRelationshipsReadStoreConfiguration>());
 
-
-//            IncludeRegistry<ReadStoreDataRegistry>();
             For<IDocumentClient>().Add(c => c.GetInstance<IDocumentClientFactory>().CreateDocumentClient()).Named(GetType().FullName).Singleton();
             For<IDocumentClientFactory>().Use<DocumentClientFactory>();
             For<IAccountProviderLegalEntitiesRepository>().Use<ReadOnlyAccountProviderLegalEntitiesRepository>().Ctor<IDocumentClient>().IsNamedInstance(GetType().FullName);
-
 
 //todo: do mediator registrations by convention
 //            IncludeRegistry<ReadStoreMediatorRegistry>();
             For<ReadStoreServiceFactory>().Use<ReadStoreServiceFactory>(c => c.GetInstance);
             For<IReadStoreMediator>().Use<ReadStoreMediator>();
-            //todo: will be required somewhere else (or convention)
-            //For<IReadStoreRequestHandler<DeletePermissionsCommand, Unit>>().Use<DeletePermissionsCommandHandler>();
             For<IReadStoreRequestHandler<HasRelationshipWithPermissionQuery, bool>>().Use<HasRelationshipWithPermissionQueryHandler>();
             For<IReadStoreRequestHandler<HasPermissionQuery, bool>>().Use<HasPermissionQueryHandler>();
-            //todo: will be required somewhere else
-            //For<IReadStoreRequestHandler<UpdatePermissionsCommand, Unit>>().Use<UpdatePermissionsCommandHandler>();
-
-
 
             For<HttpClient>().Add(c => c.GetInstance<IHttpClientFactory>().CreateHttpClient()).Named(GetType().FullName).Singleton();
             For<IHttpClientFactory>().Use<Http.HttpClientFactory>();
             For<IRestHttpClient>().Use<RestHttpClient>().Ctor<HttpClient>().IsNamedInstance(GetType().FullName);
             For<IProviderRelationshipsApiClient>().Use<ProviderRelationshipsApiClient>();
+
+            //todo: main config will contain one of these
+            For<ProviderRelationshipsReadStoreConfiguration>().Use(c => c.GetInstance<ITableStorageConfigurationService>().Get<ProviderRelationshipsReadStoreConfiguration>());
+
             For<ProviderRelationshipsApiClientConfiguration>().Use(c => c.GetInstance<ITableStorageConfigurationService>().Get<ProviderRelationshipsApiClientConfiguration>());
         }
     }
