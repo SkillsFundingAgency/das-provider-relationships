f3b24a7b9e071e4bebd973825257a5a14b689577
Phil Davies <github@phildavies.co.uk>
2018-12-14 09:22:49 +0000

    create & use a ReadOnlyDocumentRepository

--- src/SFA.DAS.ProviderRelationships.Api.Client/ReadStore/Data/AccountProviderLegalEntitiesRepository.cs
+++ src/SFA.DAS.ProviderRelationships.Api.Client/ReadStore/Data/AccountProviderLegalEntitiesRepository.cs
@@ -1,12 +1,11 @@
 using Microsoft.Azure.Documents;
-using SFA.DAS.CosmosDb;
 using SFA.DAS.ProviderRelationships.Api.Client.ReadStore.Models;
 
 namespace SFA.DAS.ProviderRelationships.Api.Client.ReadStore.Data
 {
     //todo: create a ReadOnlyDocumentRepository?
     
-    internal class AccountProviderLegalEntitiesRepository : DocumentRepository<AccountProviderLegalEntityDto>, IAccountProviderLegalEntitiesRepository
+    internal class AccountProviderLegalEntitiesRepository : ReadOnlyDocumentRepository<AccountProviderLegalEntityDto>, IAccountProviderLegalEntitiesRepository
     {
         //todo: rename other repo to include "WriteEnabled" or some such, so they have different names
 
--- src/SFA.DAS.ProviderRelationships.Api.Client/ReadStore/Data/IAccountProviderLegalEntitiesRepository.cs
+++ src/SFA.DAS.ProviderRelationships.Api.Client/ReadStore/Data/IAccountProviderLegalEntitiesRepository.cs
@@ -1,9 +1,8 @@
-using SFA.DAS.CosmosDb;
 using SFA.DAS.ProviderRelationships.Api.Client.ReadStore.Models;
 
 namespace SFA.DAS.ProviderRelationships.Api.Client.ReadStore.Data
 {
-    internal interface IAccountProviderLegalEntitiesRepository : IDocumentRepository<AccountProviderLegalEntityDto>
+    internal interface IAccountProviderLegalEntitiesRepository : IReadOnlyDocumentRepository<AccountProviderLegalEntityDto>
     {
     }
 }
