fbabddcaabff22c3166402b9982934d7580d13ac
tom-gough <tom@tgsd.co.uk>
2018-12-17 15:42:25 +0000

    Moved granted operations string construction logic to the entity

--- src/SFA.DAS.ProviderRelationships/Application/Commands/UpdatedPermissionsEventAuditCommandHandler.cs
+++ src/SFA.DAS.ProviderRelationships/Application/Commands/UpdatedPermissionsEventAuditCommandHandler.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using MediatR;
 using SFA.DAS.ProviderRelationships.Data;
 using SFA.DAS.ProviderRelationships.Models;
-using SFA.DAS.ProviderRelationships.Types.Models;
 
 namespace SFA.DAS.ProviderRelationships.Application.Commands
 {
@@ -19,22 +17,9 @@ namespace SFA.DAS.ProviderRelationships.Application.Commands
         protected override void Handle(UpdatedPermissionsEventAuditCommand request)
         {
             _db.Value.UpdatedPermissionsEventAudits.Add(new UpdatedPermissionsEventAudit(DateTime.UtcNow,
-                request.Updated, ConstructOperationsAudit(request.GrantedOperations), request.UserRef, request.Ukprn,
+                request.Updated, request.GrantedOperations, request.UserRef, request.Ukprn,
                 request.AccountProviderLegalEntityId, request.AccountProviderId, request.AccountLegalEntityId,
                 request.AccountId));
-        }
-
-        private static string ConstructOperationsAudit(IEnumerable<Operation> operations)
-        {
-            var first = true;
-            var result = string.Empty;
-            foreach (var operation in operations)
-            {
-                if (!first) { result += ","; }
-                result += ((short)operation).ToString();
-                first = false;
-            }
-            return result;
         }
     }
 }
--- src/SFA.DAS.ProviderRelationships/Models/UpdatedPermissionsEventAudit.cs
+++ src/SFA.DAS.ProviderRelationships/Models/UpdatedPermissionsEventAudit.cs
@@ -1,9 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using SFA.DAS.ProviderRelationships.Types.Models;
 
 namespace SFA.DAS.ProviderRelationships.Models
 {
     public class UpdatedPermissionsEventAudit : Entity
     {
+        public UpdatedPermissionsEventAudit(DateTime logged, DateTime updated, IEnumerable<Operation> grantedOperations, Guid userRef, long ukprn, long accountProviderLegalEntityId, long accountProviderId, long accountLegalEntityId, long accountId)
+        {
+            Logged = logged;
+            Updated = updated;
+            GrantedOperations = ConstructOperationsAudit(grantedOperations);
+            UserRef = userRef;
+            Ukprn = ukprn;
+            AccountProviderLegalEntityId = accountProviderLegalEntityId;
+            AccountProviderId = accountProviderId;
+            AccountLegalEntityId = accountLegalEntityId;
+            AccountId = accountId;
+        }
+
         public UpdatedPermissionsEventAudit(DateTime logged, DateTime updated, string grantedOperations, Guid userRef, long ukprn, long accountProviderLegalEntityId, long accountProviderId, long accountLegalEntityId, long accountId)
         {
             Logged = logged;
@@ -27,5 +42,18 @@ namespace SFA.DAS.ProviderRelationships.Models
         public string GrantedOperations { get; }
         public DateTime Updated { get; }
         public DateTime Logged { get; }
+
+        private static string ConstructOperationsAudit(IEnumerable<Operation> operations)
+        {
+            var first = true;
+            var result = string.Empty;
+            foreach (var operation in operations)
+            {
+                if (!first) { result += ","; }
+                result += ((short)operation).ToString();
+                first = false;
+            }
+            return result;
+        }
     }
 }
