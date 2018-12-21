105fbecb431210acf4106890dda29915877b20a0
Phil Davies <github@phildavies.co.uk>
2018-12-21 13:49:01 +0000

    comments transferred to the PR's description

--- src/SFA.DAS.ProviderRelationships.Api.Client/Configuration/ProviderRelationshipsApiClientConfiguration.cs
+++ src/SFA.DAS.ProviderRelationships.Api.Client/Configuration/ProviderRelationshipsApiClientConfiguration.cs
@@ -3,9 +3,6 @@ using StructureMap;
 
 namespace SFA.DAS.ProviderRelationships.Api.Client.Configuration
 {
-    //todo: (add a note to the pr description for this..) there are now 2 locations in table storage containing ReadStoreConfiguration, 1 in the api client config (used by the read-only client) and 1 in its own table for the write clients (currently message handlers and jobs)
-    // the api client will use a read-only key - plug one in and check it
-    //todo: this is going to have to go in as a V2 (with the original left in for the first release) so that there is no outage
     public class ProviderRelationshipsApiClientConfiguration
     {
         public AzureAdClientConfiguration AzureAdClient { get; set; }
