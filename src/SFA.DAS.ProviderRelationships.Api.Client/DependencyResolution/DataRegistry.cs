faee919c7d3a3acb597b3a511712404030d79d32
Phil Davies <github@phildavies.co.uk>
2019-01-07 11:51:54 +0000

    split the client's registry out into single responsibility registries

---
+++ src/SFA.DAS.ProviderRelationships.Api.Client/DependencyResolution/DataRegistry.cs
@@ -0,0 +1,16 @@
+using Microsoft.Azure.Documents;
+using StructureMap;
+using SFA.DAS.ProviderRelationships.Api.Client.ReadStore.Data;
+
+namespace SFA.DAS.ProviderRelationships.Api.Client.DependencyResolution
+{
+    internal class DataRegistry : Registry
+    {
+        public DataRegistry()
+        {
+            For<IDocumentClient>().Add(c => c.GetInstance<IDocumentClientFactory>().CreateDocumentClient()).Named(GetType().FullName).Singleton();
+            For<IDocumentClientFactory>().Use<DocumentClientFactory>();
+            For<IAccountProviderLegalEntitiesReadOnlyRepository>().Use<AccountProviderLegalEntitiesReadOnlyRepository>().Ctor<IDocumentClient>().IsNamedInstance(GetType().FullName);
+        }
+    }
+}
--- src/SFA.DAS.ProviderRelationships.Api.Client/DependencyResolution/ProviderRelationshipsApiClientRegistry.cs
+++ src/SFA.DAS.ProviderRelationships.Api.Client/DependencyResolution/ProviderRelationshipsApiClientRegistry.cs
@@ -1,14 +1,5 @@
-﻿using System.Net.Http;
-using Microsoft.Azure.Documents;
-using MediatR;
-using StructureMap;
-using SFA.DAS.AutoConfiguration;
+﻿using StructureMap;
 using SFA.DAS.AutoConfiguration.DependencyResolution;
-using SFA.DAS.ProviderRelationships.Api.Client.Configuration;
-using SFA.DAS.ProviderRelationships.Api.Client.Http;
-using SFA.DAS.ProviderRelationships.Api.Client.ReadStore.Application.Queries.HasPermission;
-using SFA.DAS.ProviderRelationships.Api.Client.ReadStore.Application.Queries.HasRelationshipWithPermission;
-using SFA.DAS.ProviderRelationships.Api.Client.ReadStore.Data;
 
 namespace SFA.DAS.ProviderRelationships.Api.Client.DependencyResolution
 {
@@ -17,26 +8,10 @@ namespace SFA.DAS.ProviderRelationships.Api.Client.DependencyResolution
         public ProviderRelationshipsApiClientRegistry()
         {
             IncludeRegistry<AutoConfigurationRegistry>();
-
-            For<IDocumentClient>().Add(c => c.GetInstance<IDocumentClientFactory>().CreateDocumentClient()).Named(GetType().FullName).Singleton();
-            For<IDocumentClientFactory>().Use<DocumentClientFactory>();
-            For<IAccountProviderLegalEntitiesReadOnlyRepository>().Use<AccountProviderLegalEntitiesReadOnlyRepository>().Ctor<IDocumentClient>().IsNamedInstance(GetType().FullName);
-
-            For<IMediator>().Use<Mediator>();
-            For<ServiceFactory>().Use<ServiceFactory>(ctx => ctx.GetInstance);
-
-            For<IRequestHandler<HasPermissionQuery, bool>>().Use<HasPermissionQueryHandler>();
-            For<IRequestHandler<HasRelationshipWithPermissionQuery, bool>>().Use<HasRelationshipWithPermissionQueryHandler>();
-
-            For<HttpClient>().Add(c => c.GetInstance<IHttpClientFactory>().CreateHttpClient()).Named(GetType().FullName).Singleton();
-            For<IHttpClientFactory>().Use<Http.HttpClientFactory>();
-            For<IRestHttpClient>().Use<RestHttpClient>().Ctor<HttpClient>().IsNamedInstance(GetType().FullName);
-            For<IProviderRelationshipsApiClient>().Use<ProviderRelationshipsApiClient>();
-
-            For<ProviderRelationshipsApiClientConfiguration>().Use(c => c.GetInstance<ITableStorageConfigurationService>()
-                .Get<ProviderRelationshipsApiClientConfiguration>(ConfigurationNames.ApiClientBase)).Singleton();
-            For<ReadStoreConfiguration>().Use(c => c.GetInstance<ProviderRelationshipsApiClientConfiguration>().ReadStore).Singleton();
-            For<AzureActiveDirectoryClientConfiguration>().Use(c => c.GetInstance<ProviderRelationshipsApiClientConfiguration>().AzureActiveDirectoryClient).Singleton();
+            IncludeRegistry<DataRegistry>();
+            IncludeRegistry<MediatorRegistry>();
+            IncludeRegistry<HttpRegistry>();
+            IncludeRegistry<ConfigurationRegistry>();
         }
     }
 }
