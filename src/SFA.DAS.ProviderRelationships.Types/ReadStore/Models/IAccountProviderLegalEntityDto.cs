88391673273df15bd61954c70b432e0ee0557e8d
Phil Davies <github@phildavies.co.uk>
2019-01-07 14:36:44 +0000

    tidy up

--- src/SFA.DAS.ProviderRelationships.Types/ReadStore/Models/IAccountProviderLegalEntityDto.cs
+++ src/SFA.DAS.ProviderRelationships.Types/ReadStore/Models/IAccountProviderLegalEntityDto.cs
@@ -7,11 +7,8 @@ namespace SFA.DAS.ProviderRelationships.Types.ReadStore.Models
     public interface IAccountProviderLegalEntityDto
     {
         long AccountLegalEntityId { get; }
-
         long Ukprn { get; }
-
         IEnumerable<Operation> Operations { get; }
-
         DateTime? Deleted { get; }
     }
 }
