d0e51259acf1192e7d84cc749cddbe72ee1a7cc3
Phil Davies <github@phildavies.co.uk>
2019-01-10 10:58:36 +0000

    remove '_' from v2 config key names

--- src/SFA.DAS.ProviderRelationships.Api.Client/Configuration/ConfigurationKeys.cs
+++ src/SFA.DAS.ProviderRelationships.Api.Client/Configuration/ConfigurationKeys.cs
@@ -3,6 +3,6 @@ namespace SFA.DAS.ProviderRelationships.Api.Client.Configuration
 {
     public static class ConfigurationKeys
     {
-        public const string ApiClientBase = "SFA.DAS.ProviderRelationships.Api.Client_V2";
+        public const string ApiClientBase = "SFA.DAS.ProviderRelationships.Api.ClientV2";
     }
 }
--- src/SFA.DAS.ProviderRelationships/Configuration/ConfigurationKeys.cs
+++ src/SFA.DAS.ProviderRelationships/Configuration/ConfigurationKeys.cs
@@ -2,7 +2,7 @@ namespace SFA.DAS.ProviderRelationships.Configuration
 {
     public static class ConfigurationKeys
     {
-        public const string ProviderRelationships = "SFA.DAS.ProviderRelationships_V2";
+        public const string ProviderRelationships = "SFA.DAS.ProviderRelationshipsV2";
         public const string EmployerFeatures = "SFA.DAS.ProviderRelationships.EmployerFeatures";
         public const string ApprenticeshipInfoServiceApi = "SFA.DAS.ApprenticeshipInfoServiceAPI";
     }
