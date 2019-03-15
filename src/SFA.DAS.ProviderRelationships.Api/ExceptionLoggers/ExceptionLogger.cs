9bbbeaa91dce44b4ea94234dd66a8dda0712a045
Elliot Ritchie <elliotritchie@hotmail.com>
2019-03-15 09:39:05 +0000

    Added exception logger

---
+++ src/SFA.DAS.ProviderRelationships.Api/ExceptionLoggers/ExceptionLogger.cs
@@ -0,0 +1,17 @@
+using System.Web.Http.ExceptionHandling;
+using SFA.DAS.NLog.Logger;
+using SFA.DAS.ProviderRelationships.Extensions;
+using WebApi.StructureMap;
+
+namespace SFA.DAS.ProviderRelationships.Api.ExceptionLoggers
+{
+    public class ExceptionLogger : System.Web.Http.ExceptionHandling.ExceptionLogger
+    {
+        public override void Log(ExceptionLoggerContext context)
+        {
+            var logger = context.Request.GetService<ILog>();
+            
+            logger.Error(context.Exception, context.Exception.GetAggregateMessage());
+        }
+    }
+}
