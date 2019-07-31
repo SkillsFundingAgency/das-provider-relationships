aa41de7d0d9d59ab05fd1d039b474cd01928e1e1
Peter Morris <mrpmorris@gmail.com>
2019-07-31 16:33:27 +0100

    ER-1444 Unit tests

---
+++ src/SFA.DAS.ProviderRelationships.UnitTests/Application/Commands/RevokePermissionsCommandHandlerTests.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using SFA.DAS.ProviderRelationships.Application.Commands.RevokePermissions;
+using SFA.DAS.ProviderRelationships.Data;
+using SFA.DAS.ProviderRelationships.Messages.Events;
+using SFA.DAS.ProviderRelationships.Models;
+using SFA.DAS.ProviderRelationships.Types.Models;
+using SFA.DAS.ProviderRelationships.UnitTests.Builders;
+using SFA.DAS.Testing;
+using SFA.DAS.UnitOfWork;
+
+namespace SFA.DAS.ProviderRelationships.UnitTests.Application.Commands
+{
+    [TestFixture]
+    public class RevokePermissionsCommandHandlerTests : FluentTest<RevokePermissionsCommandHandlerTestsFixture>
+    {
+        [Test]
+        [Parallelizable]
+        public Task WhenExecuted_ThenShouldRemoveMatchingOperationsOnly() =>
+            RunAsync(
+                act: async f =>
+                {
+                    await f.Handler.Handle(f.RevokePermissionsCommand, CancellationToken.None);
+                },
+                assert: f =>
+                {
+                    f.AccountProviderLegalEntity
+                        .Permissions
+                        .Select(x => x.Operation)
+                        .Should()
+                        .BeEquivalentTo(new[] { Operation.CreateCohort });
+                }
+            );
+
+        [Test]
+        [Parallelizable]
+        public Task WhenExecuted_ThenShouldPublishEvents() =>
+            RunAsync(
+                act: async f =>
+                {
+                    await f.Handler.Handle(f.RevokePermissionsCommand, CancellationToken.None);
+                },
+                assert: f =>
+                {
+                    IEnumerable<object> events = f.UnitOfWorkContext.GetEvents();
+                    events.Should().HaveCount(1);
+
+                    var firstEvent = events
+                        .OfType<UpdatedPermissionsEvent>()
+                        .Single();
+                    firstEvent.AccountId.Should().Be(f.Account.Id);
+                    firstEvent.AccountLegalEntityId.Should().Be(f.AccountLegalEntity.Id);
+                    firstEvent.AccountProviderId.Should().Be(f.AccountProvider.Id);
+                    firstEvent.AccountProviderLegalEntityId.Should().Be(f.AccountProviderLegalEntity.Id);
+                    firstEvent.Ukprn.Should().Be(f.Provider.Ukprn);
+                    firstEvent.UserRef.Should().Be(Guid.Empty);
+                }
+            );
+
+    }
+
+    public class RevokePermissionsCommandHandlerTestsFixture
+    {
+        public RevokePermissionsCommand RevokePermissionsCommand;
+        public IRequestHandler<RevokePermissionsCommand, Unit> Handler { get; set; }
+        public ProviderRelationshipsDbContext Db { get; set; }
+        public IUnitOfWorkContext UnitOfWorkContext { get; set; }
+
+        public Account Account;
+        public Provider Provider;
+        public User User;
+        public AccountLegalEntity AccountLegalEntity;
+        public AccountProvider AccountProvider;
+        public AccountProviderLegalEntity AccountProviderLegalEntity;
+
+        public RevokePermissionsCommandHandlerTestsFixture()
+        {
+            UnitOfWorkContext = new UnitOfWorkContext();
+
+            CreateDb();
+            CreateDefaultEntities();
+
+            var lazyDb = new Lazy<ProviderRelationshipsDbContext>(() => Db);
+            Handler = new RevokePermissionsCommandHandler(lazyDb);
+
+            RevokePermissionsCommand = new RevokePermissionsCommand(
+                ukprn: 299792458,
+                accountLegalEntityPublicHashedId: "ALE1",
+                operationsToRemove: new[] { Operation.Recruitment });
+
+            UnitOfWorkContext = new UnitOfWorkContext();
+        }
+
+        private void CreateDb()
+        {
+            var optionsBuilder =
+                new DbContextOptionsBuilder<ProviderRelationshipsDbContext>()
+                    .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                    .ConfigureWarnings(warnings =>
+                        warnings.Throw(RelationalEventId.QueryClientEvaluationWarning)
+                    );
+            Db = new ProviderRelationshipsDbContext(optionsBuilder.Options);
+        }
+
+        private void CreateDefaultEntities()
+        {
+            Account = new Account(
+                id: 1,
+                hashedId: "HashedId",
+                publicHashedId: "PublicHashedId",
+                name: "Account",
+                created: DateTime.UtcNow);
+            Db.Add(Account);
+
+            AccountLegalEntity = new AccountLegalEntity(
+                account: Account,
+                id: 12345,
+                publicHashedId: "ALE1",
+                name: "Account legal entity 1",
+                created: DateTime.UtcNow);
+            Db.Add(AccountLegalEntity);
+
+            Provider = EntityActivator
+                .CreateInstance<Provider>()
+                .Set(x => x.Ukprn, 299792458);
+            Db.Add(Provider);
+
+            User = new User(Guid.NewGuid(), "me@home.com", "Bill", "Gates");
+            Db.Add(User);
+
+            AccountProvider = new AccountProvider(Account, Provider, User);
+            AccountProvider.Set(x => x.Id, 23);
+            Db.Add(AccountProvider);
+
+            AccountProviderLegalEntity = new AccountProviderLegalEntity(
+                accountProvider: AccountProvider,
+                accountLegalEntity: AccountLegalEntity,
+                user: User,
+                grantedOperations: new HashSet<Operation>(new[] { Operation.CreateCohort, Operation.Recruitment }));
+            AccountProviderLegalEntity.Set(x => x.Id, 34);
+            Db.Add(AccountProviderLegalEntity);
+
+            Db.SaveChanges();
+        }
+    }
+}
