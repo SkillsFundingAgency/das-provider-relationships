214251f96d7a375b8d0624096b025b5e4f7bbc64
Paul Howes <paul.howes@cognetics.net>
2019-09-24 15:51:55 +0100

    AML-3832 Start New Account Page

---
+++ src/SFA.DAS.ProviderRegistrations.Web/Controllers/RegistrationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.ProviderRegistrations.Web.Controllers
+{
+    [Route("registration")]
+    public class RegistrationController : Controller
+    {
+        [HttpGet]
+        [Route("startAccountSetup")]
+        public IActionResult StartAccountSetup()
+        {
+            return View();
+        }
+    }
+}
