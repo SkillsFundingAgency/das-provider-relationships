bf99188ab7927c39c5e038e49c95a88c3f4c4857
Corey <corey.faulconbridge@digital.education.gov.uk>
2020-03-11 09:28:25 +0000

    CON-1533 Add HtmlHelper in order to pull the Cdn base url from config

--- src/SFA.DAS.ProviderRelationships.Web/Extensions/HtmlHelperExtensions.cs
+++ src/SFA.DAS.ProviderRelationships.Web/Extensions/HtmlHelperExtensions.cs
@@ -1,4 +1,6 @@
 using System.Web.Mvc;
+using SFA.DAS.ProviderRelationships.Configuration;
+using SFA.DAS.ProviderRelationships.Web.App_Start;
 using SFA.DAS.ProviderRelationships.Web.Urls;
 
 namespace SFA.DAS.ProviderRelationships.Web.Extensions
@@ -8,6 +10,14 @@ namespace SFA.DAS.ProviderRelationships.Web.Extensions
         public static IEmployerUrls EmployerUrls(this HtmlHelper htmlHelper)
         {
             return (IEmployerUrls)htmlHelper.ViewBag.EmployerUrls;
+        }
+
+        public static MvcHtmlString CdnLink(this HtmlHelper html, string folderName, string fileName)
+        {
+            var cdnLocation = StructuremapMvc.StructureMapDependencyScope.Container.GetInstance<ProviderRelationshipsConfiguration>().CdnBaseUrl;
+
+            var trimCharacters = new char[] { '/' };
+            return new MvcHtmlString($"{cdnLocation.Trim(trimCharacters)}/{folderName.Trim(trimCharacters)}/{fileName.Trim(trimCharacters)}");
         }
     }
 }
