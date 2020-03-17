7552013123fede968fd913035565120dae1988ef
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2020-03-17 15:40:02 +0000

    CON-1466-revert to ProviderUkprn

--- src/SFA.DAS.ProviderRelationships.Types/Dtos/AccountProviderDto.cs
+++ src/SFA.DAS.ProviderRelationships.Types/Dtos/AccountProviderDto.cs
@@ -5,7 +5,7 @@ namespace SFA.DAS.ProviderRelationships.Types.Dtos
     public class AccountProviderDto
     {
         public long Id { get; set; }
-        public long Ukprn { get; set; }
+        public long ProviderUkprn { get; set; }
         public string ProviderName { get; set; }
         public List<AccountLegalEntityDto> AccountLegalEntities { get; set; }
         public string FormattedProviderSuggestion { get; set; }
