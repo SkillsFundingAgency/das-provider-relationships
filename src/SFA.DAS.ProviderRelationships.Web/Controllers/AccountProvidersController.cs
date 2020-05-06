def154f2823b2e31ac73e66885dddebb39cc5c67
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2020-05-06 13:41:36 +0100

    CON-1466-fix build

--- src/SFA.DAS.ProviderRelationships.Web/Controllers/AccountProvidersController.cs
+++ src/SFA.DAS.ProviderRelationships.Web/Controllers/AccountProvidersController.cs
@@ -19,7 +19,6 @@ using SFA.DAS.ProviderRelationships.Web.RouteValues.AccountProviderLegalEntities
 using SFA.DAS.ProviderRelationships.Web.RouteValues.AccountProviders;
 using SFA.DAS.ProviderRelationships.Web.Urls;
 using SFA.DAS.ProviderRelationships.Web.ViewModels.AccountProviders;
-using SFA.DAS.ProviderRelationships.Web.ViewModels.Providers;
 using SFA.DAS.Validation.Mvc;
 
 namespace SFA.DAS.ProviderRelationships.Web.Controllers
@@ -226,42 +225,6 @@ namespace SFA.DAS.ProviderRelationships.Web.Controllers
             var accountProviderId = await _mediator.Send(new AddAccountProviderCommand(routeValues.AccountId.Value, invitation.Invitation.Ukprn, routeValues.UserRef.Value, routeValues.CorrelationId));
 
             return RedirectToAction("Get", new GetAccountProviderRouteValues { AccountProviderId = accountProviderId });
-        }
-
-        [DasAuthorize(EmployerUserRole.Owner)]
-        [Route("find")]
-        public async Task<ActionResult> Find()
-        {
-            var query = new FindAllProvidersQuery();
-            var result = await _mediator.Send(query);
-            var model = _mapper.Map<FindProvidersViewModel>(result);
-            return View(model);
-        }
-
-        [DasAuthorize(EmployerUserRole.Owner)]
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Route("find")]
-        public async Task<ActionResult> Find(FindProvidersViewModel model)
-        {
-            var ukprn = long.Parse(model.Ukprn);
-            var query = new FindProviderToAddQuery(model.AccountId.Value, ukprn);
-
-            var result = await _mediator.Send(query);
-
-            if (result.ProviderNotFound)
-            {
-                ModelState.AddModelError(nameof(model.Ukprn), ErrorMessages.InvalidUkprn);
-
-                return RedirectToAction("Find");
-            }
-
-            if (result.ProviderAlreadyAdded)
-            {
-                return RedirectToAction("AlreadyAdded", new AlreadyAddedAccountProviderRouteValues { AccountProviderId = result.AccountProviderId.Value });
-            }
-
-            return RedirectToAction("Add", new AddAccountProviderRouteValues { Ukprn = result.Ukprn });
         }
     }
 }
