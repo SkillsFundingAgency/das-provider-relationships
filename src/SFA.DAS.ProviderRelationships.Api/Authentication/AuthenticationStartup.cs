408c990507374bd13b412a390f33e41743d12593
vas <vasantha.kasirajan@digital.education.gov.uk>
2020-07-30 13:37:30 +0100

    CON-2156-build fix

--- src/SFA.DAS.ProviderRelationships.Api/Authentication/AuthenticationStartup.cs
+++ src/SFA.DAS.ProviderRelationships.Api/Authentication/AuthenticationStartup.cs
@@ -1,8 +1,8 @@
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.Owin.Security.ActiveDirectory;
 using Owin;
 using SFA.DAS.NLog.Logger;
 using SFA.DAS.ProviderRelationships.Configuration;
+using System.IdentityModel.Tokens;
 
 namespace SFA.DAS.ProviderRelationships.Api.Authentication
 {
@@ -29,8 +29,8 @@ namespace SFA.DAS.ProviderRelationships.Api.Authentication
             _app.UseWindowsAzureActiveDirectoryBearerAuthentication(new WindowsAzureActiveDirectoryBearerAuthenticationOptions
             {
                 Tenant = _config.Tenant,
-                TokenValidationParameters = new TokenValidationParameters
-                {
+                TokenValidationParameters = new TokenValidationParameters {
+
                     RoleClaimType = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role",
                     ValidAudience = _config.Audience
                 }
