0941a74239cc1ccfbbba7837d3e795f61e25d895
Paul Howes <paul.howes@cognetics.net>
2021-04-12 11:55:30 +0100

    Update OperationExtensionsTests.cs

--- src/SFA.DAS.ProviderRelationships.Web.UnitTests/Extensions/OperationExtensionsTests.cs
+++ src/SFA.DAS.ProviderRelationships.Web.UnitTests/Extensions/OperationExtensionsTests.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
 using FluentAssertions;
+using MoreLinq;
 using NUnit.Framework;
 using SFA.DAS.ProviderRelationships.Types.Models;
 using SFA.DAS.ProviderRelationships.Web.Extensions;
+using SFA.DAS.ProviderRelationships.Web.ViewModels.Permissions;
 using SFA.DAS.Testing;
 
 namespace SFA.DAS.ProviderRelationships.Web.UnitTests.Extensions
@@ -16,5 +19,72 @@ namespace SFA.DAS.ProviderRelationships.Web.UnitTests.Extensions
             Run(f => Permission.CreateCohort.GetDescription(), (f, r) => r.Should().Be("Add apprentice records"));
             Run(f => Permission.Recruitment.GetDescription(), (f, r) => r.Should().Be("Recruit apprentices"));
         }
+
+        [TestCaseSource(nameof(OperationToPermissions))]
+        public void ToPermissions_WhenMappingOperationsToPermissions_ThenShouldMapCorrectly(List<Operation> operations, List<PermissionViewModel> permissions)
+        {
+            var result = operations.ToPermissions();
+
+            CollectionAssert.AreEquivalent(permissions, result);
+        }
+
+        [TestCaseSource(nameof(OperationToPermissions))]
+        public void ToOperations_WhenMappingOperationsToPermissions_ThenShouldMapCorrectly(List<Operation> operations, List<PermissionViewModel> permissions)
+        {
+            var result = permissions.ToOperations();
+
+            CollectionAssert.AreEquivalent(operations.ToHashSet(), result);
+        }
+
+        private static readonly object[] OperationToPermissions = {
+            new object[] {
+                new List<Operation> (),
+                new List<PermissionViewModel>
+                {
+                    new PermissionViewModel {Value = Permission.Recruitment, State = State.No},
+                    new PermissionViewModel {Value = Permission.CreateCohort, State = State.No}
+                }
+            },
+            new object[] {
+                new List<Operation> {Operation.Recruitment},
+                new List<PermissionViewModel>
+                {
+                    new PermissionViewModel {Value = Permission.Recruitment, State = State.Yes},
+                    new PermissionViewModel {Value = Permission.CreateCohort, State = State.No}
+                }
+            },
+            new object[] {
+                new List<Operation> {Operation.CreateCohort},
+                new List<PermissionViewModel>
+                {
+                    new PermissionViewModel {Value = Permission.Recruitment, State = State.No},
+                    new PermissionViewModel {Value = Permission.CreateCohort, State = State.Yes}
+                }
+            },
+            new object[] {
+                new List<Operation> {Operation.Recruitment, Operation.CreateCohort},
+                new List<PermissionViewModel>
+                {
+                    new PermissionViewModel {Value = Permission.Recruitment, State = State.Yes},
+                    new PermissionViewModel {Value = Permission.CreateCohort, State = State.Yes}
+                }
+            },
+            new object[] {
+                new List<Operation> {Operation.Recruitment, Operation.RecruitmentRequiresReview},
+                new List<PermissionViewModel>
+                {
+                    new PermissionViewModel {Value = Permission.Recruitment, State = State.Conditional},
+                    new PermissionViewModel {Value = Permission.CreateCohort, State = State.No}
+                }
+            },
+            new object[] {
+                new List<Operation> {Operation.CreateCohort, Operation.RecruitmentRequiresReview, Operation.Recruitment},
+                new List<PermissionViewModel>
+                {
+                    new PermissionViewModel {Value = Permission.Recruitment, State = State.Conditional},
+                    new PermissionViewModel {Value = Permission.CreateCohort, State = State.Yes}
+                }
+            }
+        };
     }
 }
