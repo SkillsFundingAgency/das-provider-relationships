a92c7a228221325342286682ee1f0b66e78fa01b
Paul Howes <paul.howes@cognetics.net>
2021-04-16 14:41:07 +0100

    Update PermissionViewModel.cs

--- src/SFA.DAS.ProviderRelationships.Web/ViewModels/Permissions/PermissionViewModel.cs
+++ src/SFA.DAS.ProviderRelationships.Web/ViewModels/Permissions/PermissionViewModel.cs
@@ -1,24 +1,21 @@
-﻿using System;
-using SFA.DAS.ProviderRelationships.Types.Models;
+﻿using SFA.DAS.ProviderRelationships.Types.Models;
 
 namespace SFA.DAS.ProviderRelationships.Web.ViewModels.Permissions
 {
-    [Serializable]
     public class PermissionViewModel
     {
         public State State { get; set; }
         public Permission Value { get; set; }
 
-        public override bool Equals(object obj)
-        {
-            var operationViewModel = obj as PermissionViewModel;
-
-            return Equals(operationViewModel);
-        }
-
         public override int GetHashCode()
         {
-            return ((short)Value).GetHashCode();
+            return Value.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is PermissionViewModel compare)) return false;
+            return State.Equals(compare.State) && Value.Equals(compare.Value);
         }
     }
 }
