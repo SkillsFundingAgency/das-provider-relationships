e4a38adf18e9bec1afe6a7b602c8d15ab4d637ec
Paul Howes <paul.howes@cognetics.net>
2021-04-22 17:06:41 +0100

    Added feature toggle

--- src/SFA.DAS.ProviderRelationships.Web/App_Start/FilterConfig.cs
+++ src/SFA.DAS.ProviderRelationships.Web/App_Start/FilterConfig.cs
@@ -18,6 +18,7 @@ namespace SFA.DAS.ProviderRelationships.Web
             filters.AddValidationFilter();
             filters.Add(new GoogleAnalyticsViewBagFilter(() => DependencyResolver.Current.GetService<GoogleAnalyticsConfiguration>()));
             filters.Add(new UrlsViewBagFilter(() => DependencyResolver.Current.GetService<IEmployerUrls>()));
+            filters.Add(new ConfigurationViewBagFilter(() => DependencyResolver.Current.GetService<ProviderRelationshipsConfiguration>()));
         }
     }    
 }
