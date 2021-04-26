4deb5e39e92e67f2a23b4dc7db0d0e9ce8c4f09e
Paul Howes <paul.howes@cognetics.net>
2021-04-26 10:01:10 +0100

    Updated API client

--- src/SFA.DAS.ProviderRelationships.Types/Dtos/GetAccountProviderLegalEntitiesWithPermissionRequest.cs
+++ src/SFA.DAS.ProviderRelationships.Types/Dtos/GetAccountProviderLegalEntitiesWithPermissionRequest.cs
@@ -4,6 +4,8 @@ namespace SFA.DAS.ProviderRelationships.Types.Dtos
 {
     public class GetAccountProviderLegalEntitiesWithPermissionRequest
     {
+        public string AccountHashedId { get; set; }
+        public string AccountLegalEntityPublicHashedId { get; set; }
         public long Ukprn { get; set; }
         public Operation Operation { get; set; }
     }
