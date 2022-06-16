4d85f6c7af887a06b3168b91304935577be2d2fe
Corey <corey.faulconbridge@digital.education.gov.uk>
2022-06-16 14:19:50 +0100

    [MAP-75] Remove a load of unnecessary code

--- src/SFA.DAS.ProviderRelationships.Web/Urls/IEmployerUrls.cs
+++ src/SFA.DAS.ProviderRelationships.Web/Urls/IEmployerUrls.cs
@@ -5,50 +5,11 @@ namespace SFA.DAS.ProviderRelationships.Web.Urls
         void Initialize(string accountHashedId);
         
         #region Accounts
-
         string Account(string hashedAccountId = null);
-        string NotificationSettings();
-        string PayeSchemes(string hashedAccountId = null);
-        string RenameAccount(string hashedAccountId = null);
-        string YourAccounts();
-        string YourOrganisationsAndAgreements(string hashedAccountId = null);
-        string YourTeam(string hashedAccountId = null);
-
         #endregion Accounts
 
-        #region Commitments
-
-        string Apprentices(string hashedAccountId = null);
-
-        #endregion Commitments
-
-        #region Finance
-        
-        string Finance(string hashedAccountId = null);
-        
-        #endregion Finance
-
         #region Portal
-
-        string Help();
         string Homepage();
-        string Privacy();
-        string SignIn();
-        string SignOut();
-
         #endregion Portal
-
-        #region Recruit
-        
-        string Recruit(string hashedAccountId = null);
-        
-        #endregion Recruit
-
-        #region Users
-        
-        string ChangeEmail();
-        string ChangePassword();
-
-        #endregion Users
     }
 }
