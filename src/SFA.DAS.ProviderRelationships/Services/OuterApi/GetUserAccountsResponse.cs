8af714963c582adf3e8eeace1b902abd119e7158
Scott Wakefield <scott@expertcc.co.uk>
2022-11-14 22:21:47 +0000

    FAI-628 remove not used properties

--- src/SFA.DAS.ProviderRelationships/Services/OuterApi/GetUserAccountsResponse.cs
+++ src/SFA.DAS.ProviderRelationships/Services/OuterApi/GetUserAccountsResponse.cs
@@ -6,9 +6,6 @@ namespace SFA.DAS.ProviderRelationships.Services.OuterApi
 {
     public class GetUserAccountsResponse
     {
-        [JsonProperty] public string EmployerUserId { get; set; }
-        [JsonProperty] public string FirstName { get; set; }
-        [JsonProperty] public string LastName { get; set; }
         [JsonProperty] public List<EmployerIdentifier> UserAccounts { get; set; }
     }
 }
