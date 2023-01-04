09188b01c17e8c5034b127d9501c1331be23f6ba
Scott Wakefield <scott@expertcc.co.uk>
2023-01-04 09:24:21 +0000

    FAI-628 get projs on same ver of mapper, adjust registry as a result.

--- src/SFA.DAS.ProviderRelationships/DependencyResolution/MapperRegistry.cs
+++ src/SFA.DAS.ProviderRelationships/DependencyResolution/MapperRegistry.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
 using StructureMap;
@@ -9,7 +10,17 @@ namespace SFA.DAS.ProviderRelationships.DependencyResolution
     {
         public MapperRegistry()
         {
-            For<IConfigurationProvider>().Use(c => new MapperConfiguration(cfg => cfg.AddProfiles(AppDomain.CurrentDomain.GetAssemblies().Where(a => a.FullName.StartsWith("SFA.DAS"))))).Singleton();
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies()
+                .Where(a => a.FullName.StartsWith("SFA.DAS"));
+            var profiles = new List<Profile>();
+            foreach (var assembly in assemblies)
+            {
+                foreach (var profile in assembly.GetTypes().Where(x => typeof(Profile).IsAssignableFrom(x)))
+                {
+                    profiles.Add(Activator.CreateInstance(profile) as Profile);
+                }
+            }
+            For<IConfigurationProvider>().Use(c => new MapperConfiguration(cfg => cfg.AddProfiles(profiles))).Singleton();
             For<IMapper>().Use(c => c.GetInstance<IConfigurationProvider>().CreateMapper()).Singleton();
         }
     }
