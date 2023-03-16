6ac8dc7c81ff5f50761c3549a76b07d315dee154
Jon Harris <jon.harris@digital.education.gov.uk>
2023-03-16 15:25:07 +0000

    Code tidy

--- src/SFA.DAS.ProviderRelationships.Web/Controllers/ServiceController.cs
+++ src/SFA.DAS.ProviderRelationships.Web/Controllers/ServiceController.cs
@@ -1,34 +1,33 @@
 ﻿using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 
-namespace SFA.DAS.ProviderRelationships.Web.Controllers
+namespace SFA.DAS.ProviderRelationships.Web.Controllers;
+
+public class ServiceController : Controller
 {
-    public class ServiceController : Controller
+    [Route("signout")]
+    [AllowAnonymous]
+    public async Task<IActionResult> SignOutEmployer()
     {
-        [Route("provider-signout")]
-        [AllowAnonymous]
-        public async Task<IActionResult> SignOutEmployer()
-        {
-            var idToken = await HttpContext.GetTokenAsync("id_token");
+        var idToken = await HttpContext.GetTokenAsync("id_token");
 
-            var authenticationProperties = new AuthenticationProperties();
-            authenticationProperties.Parameters.Clear();
-            authenticationProperties.Parameters.Add("id_token",idToken);
+        var authenticationProperties = new AuthenticationProperties();
+        authenticationProperties.Parameters.Clear();
+        authenticationProperties.Parameters.Add("id_token",idToken);
             
-            return SignOut(authenticationProperties, CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme);
-        }
+        return SignOut(authenticationProperties, CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme);
+    }
 
-        [Route("signoutcleanup")]
-        [AllowAnonymous]
-        public async Task SignOutCleanup()
-        {
-            var idToken = await HttpContext.GetTokenAsync("id_token");
+    [Route("signoutcleanup")]
+    [AllowAnonymous]
+    public async Task SignOutCleanup()
+    {
+        var idToken = await HttpContext.GetTokenAsync("id_token");
 
-            var authenticationProperties = new AuthenticationProperties();
-            authenticationProperties.Parameters.Clear();
-            authenticationProperties.Parameters.Add("id_token", idToken);
+        var authenticationProperties = new AuthenticationProperties();
+        authenticationProperties.Parameters.Clear();
+        authenticationProperties.Parameters.Add("id_token", idToken);
 
-            SignOut(authenticationProperties, CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme);
-        }
+        SignOut(authenticationProperties, CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme);
     }
 }
