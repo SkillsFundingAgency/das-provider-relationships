2b4a618e7d3701bff4ed146ca2b0667774c5bbff
Jon Harris <jon.harris@digital.education.gov.uk>
2023-04-03 16:55:32 +0100

    Fixing GetRequestUrlRoot

--- src/SFA.DAS.ProviderRelationships.Web/Extensions/HtmlHelperExtensions.cs
+++ src/SFA.DAS.ProviderRelationships.Web/Extensions/HtmlHelperExtensions.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using SFA.DAS.MA.Shared.UI.Configuration;
 using SFA.DAS.MA.Shared.UI.Models;
@@ -6,31 +5,31 @@ using SFA.DAS.ProviderRelationships.Configuration;
 using SFA.DAS.ProviderRelationships.Web.RouteValues;
 using SFA.DAS.ProviderRelationships.Web.Urls;
 
-namespace SFA.DAS.ProviderRelationships.Web.Extensions
-{
-    public static class HtmlHelperExtensions
-    {
-        public static IEmployerUrls EmployerUrls(this IHtmlHelper htmlHelper)
-        {
-            return (IEmployerUrls)htmlHelper.ViewBag.EmployerUrls;
-        }
+namespace SFA.DAS.ProviderRelationships.Web.Extensions;
 
-        public static ICookieBannerViewModel GetCookieBannerViewModel(this IHtmlHelper html, IEmployerUrlsConfiguration configuration)
-        {
-            return new CookieBannerViewModel(new CookieBannerConfiguration {
+public static class HtmlHelperExtensions
+{
+    public static IEmployerUrls EmployerUrls(this IHtmlHelper htmlHelper)
+    {
+        return (IEmployerUrls)htmlHelper.ViewBag.EmployerUrls;
+    }
+
+    public static ICookieBannerViewModel GetCookieBannerViewModel(this IHtmlHelper html, IEmployerUrlsConfiguration configuration)
+    {
+        return new CookieBannerViewModel(new CookieBannerConfiguration {
                 ManageApprenticeshipsBaseUrl = configuration.EmployerAccountsBaseUrl
             },
             new UserContext {
                 User = html.ViewContext.HttpContext.User,
                 HashedAccountId = html.ViewContext.RouteData.Values[RouteValueKeys.AccountHashedId]?.ToString()
             });
-        }
+    }
 
-        public static IHeaderViewModel GetHeaderViewModel(this IHtmlHelper html, IEmployerUrlsConfiguration configuration, IOidcConfiguration oidcConfiguration)
-        {
-            var requestRoot = GetRootUrl(html.ViewContext.HttpContext.Request);
+    public static IHeaderViewModel GetHeaderViewModel(this IHtmlHelper html, IEmployerUrlsConfiguration configuration, IOidcConfiguration oidcConfiguration)
+    {
+        var requestRoot = GetRequestUrlRoot(html.ViewContext.HttpContext.Request);
 
-            var headerModel = new HeaderViewModel(new HeaderConfiguration {
+        var headerModel = new HeaderViewModel(new HeaderConfiguration {
                 ManageApprenticeshipsBaseUrl = configuration.EmployerAccountsBaseUrl,
                 ApplicationBaseUrl = configuration.EmployerAccountsBaseUrl,
                 EmployerCommitmentsV2BaseUrl = configuration.EmployerCommitmentsBaseUrl,
@@ -47,14 +46,14 @@ namespace SFA.DAS.ProviderRelationships.Web.Extensions
                 HashedAccountId = html.ViewContext.RouteData.Values[RouteValueKeys.AccountHashedId]?.ToString()
             });
 
-            headerModel.SelectMenu("home");
+        headerModel.SelectMenu("home");
 
-            return headerModel;
-        }
+        return headerModel;
+    }
 
-        public static IFooterViewModel GetFooterViewModel(this IHtmlHelper html, IEmployerUrlsConfiguration configuration)
-        {
-            return new FooterViewModel(new FooterConfiguration {
+    public static IFooterViewModel GetFooterViewModel(this IHtmlHelper html, IEmployerUrlsConfiguration configuration)
+    {
+        return new FooterViewModel(new FooterConfiguration {
                 ManageApprenticeshipsBaseUrl = configuration.EmployerAccountsBaseUrl,
                 AuthenticationAuthorityUrl = configuration.EmployerUsersBaseUrl
             },
@@ -62,13 +61,10 @@ namespace SFA.DAS.ProviderRelationships.Web.Extensions
                 User = html.ViewContext.HttpContext.User,
                 HashedAccountId = html.ViewContext.RouteData.Values[RouteValueKeys.AccountHashedId]?.ToString()
             });
-        }
+    }
 
-        private static string GetRootUrl(HttpRequest request)
-        {
-            var requestUrl = new Uri(request.Host.ToUriComponent());
-
-            return $"{request.Scheme}://{requestUrl.AbsoluteUri}";
-        }
+    private static string GetRequestUrlRoot(HttpRequest request)
+    {
+        return $"{request.Scheme}://{request.Host}";
     }
 }
