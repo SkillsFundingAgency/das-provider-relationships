b4fa96aaf2baaf14b92a2100f969323bc67eb101
Jon Harris <jon.harris@digital.education.gov.uk>
2023-04-18 07:45:12 +0100

    Empty array improvement

--- src/SFA.DAS.ProviderRelationships.Api/ServiceRegistrations/SwaggerServiceRegistrations.cs
+++ src/SFA.DAS.ProviderRelationships.Api/ServiceRegistrations/SwaggerServiceRegistrations.cs
@@ -29,7 +29,7 @@ public static class SwaggerServiceRegistrations
                             Id = "bearerAuth"
                         }
                     },
-                    new string[] {}
+                    Array.Empty<string>()
                 }
             };
 
