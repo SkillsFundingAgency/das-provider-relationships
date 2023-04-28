429338741f9db26198449be5c09e8ba5f4bbd8f1
Jon Harris <jon.harris@digital.education.gov.uk>
2023-04-28 08:11:45 +0100

    Removing EF and NserviceBus unit of work startup

--- src/SFA.DAS.ProviderRelationships.Api/Startup.cs
+++ src/SFA.DAS.ProviderRelationships.Api/Startup.cs
@@ -11,10 +11,7 @@ using SFA.DAS.ProviderRelationships.Api.Handlers;
 using SFA.DAS.ProviderRelationships.Api.ServiceRegistrations;
 using SFA.DAS.ProviderRelationships.Application.Commands.RevokePermissions;
 using SFA.DAS.ProviderRelationships.Configuration;
-using SFA.DAS.ProviderRelationships.Data;
 using SFA.DAS.ProviderRelationships.ServiceRegistrations;
-using SFA.DAS.UnitOfWork.EntityFrameworkCore.DependencyResolution.Microsoft;
-using SFA.DAS.UnitOfWork.NServiceBus.Features.ClientOutbox.DependencyResolution.Microsoft;
 
 namespace SFA.DAS.ProviderRelationships.Api;
 
@@ -37,26 +34,28 @@ public class Startup
         var providerRelationshipsConfiguration = _configuration.Get<ProviderRelationshipsConfiguration>();
         var isDevelopment = _configuration.IsDevOrLocal();
 
+        services.AddApiAuthentication(_configuration, isDevelopment)
+                .AddApiAuthorization(isDevelopment);
+
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining(typeof(RevokePermissionsCommand)));
 
         services.AddDatabaseRegistration(providerRelationshipsConfiguration.DatabaseConnectionString);
         services.AddApplicationServices();
         services.AddReadStoreServices();
-        services.AddEntityFrameworkUnitOfWork<ProviderRelationshipsDbContext>();
-        services.AddNServiceBusClientUnitOfWork();
+
+        //services.AddEntityFrameworkUnitOfWork<ProviderRelationshipsDbContext>();
+        //services.AddNServiceBusClientUnitOfWork();
+
         services.AddTransient<IClientOutboxStorageV2, ClientOutboxPersisterV2>();
 
-        services.AddApiAuthentication(_configuration, isDevelopment);
-        services.AddApiAuthorization(isDevelopment);
-
         services.AddSwaggerGen(c =>
-        {
-            c.OperationFilter<AuthorizationHeaderParameterOperationFilter>();
-            c.SwaggerDoc("v1", new OpenApiInfo {
-                Version = "v1",
-                Title = "Provider Relationships API"
-            });
-        });
+  {
+      c.OperationFilter<AuthorizationHeaderParameterOperationFilter>();
+      c.SwaggerDoc("v1", new OpenApiInfo {
+          Version = "v1",
+          Title = "Provider Relationships API"
+      });
+  });
 
         services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
 
